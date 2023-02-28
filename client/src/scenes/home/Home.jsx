f6815ab2a65a3bc6692c79c245297d90d04f9963
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-02-28 23:28:51 +0530

    added Dashbord Scene

---
+++ client/src/scenes/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div>
+      <Link to="/admin">Hello</Link>
+    </div>
+  );
+};
+
+export default Home;
