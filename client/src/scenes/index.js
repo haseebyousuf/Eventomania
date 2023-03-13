684a4274d99f268d0ce5e075655df1d1d1022b52
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-03-13 22:23:54 +0530

    added default export

---
+++ client/src/scenes/index.js
@@ -0,0 +1,9 @@
+export {default as About} from "./about/About";
+export {default as AddCommiittees} from "./addCommittees/AddCommiittees";
+export {default as AllCommittees} from "./allCommittees/AllCommittees";
+export {default as Committees} from "./committees/Committees";
+export {default as Dashboard} from "./dashboard/Dashboard";
+export {default as Home} from "./home/Home";
+export {default as Layout} from "./layout/Layout";
+export {default as Login} from "./login/Login";
+export {default as ViewCommittee} from "./viewCommittee/ViewCommittee";
