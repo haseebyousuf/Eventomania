c89fc7abd30d245062db47cb22cb211419a86359
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-03-13 22:23:33 +0530

    added view Committees pages

---
+++ client/src/scenes/viewCommittee/ViewCommittee.jsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
+
+const ViewCommittee = () => {
+  return <div>ViewCommittee</div>;
+};
+
+export default ViewCommittee;
