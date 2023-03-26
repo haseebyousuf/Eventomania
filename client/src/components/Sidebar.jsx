a9e7aeb428ba2f6b460904ee867a88d94a3c9bcc
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-03-26 23:24:37 +0530

    modified sidebar based on user role

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -10,11 +10,12 @@ import {
   ListItemButton,
   ListItemIcon,
   ListItemText,
+  Tooltip,
   Typography,
   useTheme,
+  Zoom,
 } from "@mui/material";
 import {
-  SettingsOutlined,
   EventRepeatOutlined,
   EventOutlined,
   VisibilityOutlined,
@@ -22,20 +23,15 @@ import {
   ChevronLeft,
   ChevronRightOutlined,
   HomeOutlined,
-  Groups2Outlined,
-  ReceiptLongOutlined,
-  PublicOutlined,
-  PointOfSaleOutlined,
-  TodayOutlined,
-  CalendarMonthOutlined,
-  AdminPanelSettingsOutlined,
-  TrendingUpOutlined,
 } from "@mui/icons-material";
+import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
 
 import FlexBetween from "./FlexBetween";
 import Profile from "assets/profile.png";
+import { useDispatch, useSelector } from "react-redux";
+import { setLogout } from "state";
 
-const navItems = [
+const adminNavItems = [
   {
     text: "Dashboard",
     icon: <HomeOutlined />,
@@ -73,7 +69,49 @@ const navItems = [
     icon: <VisibilityOutlined />,
   },
   {
+    text: "Add Convenors",
+    icon: <VisibilityOutlined />,
+  },
+  {
     text: "Members",
+    icon: <ControlPointOutlined />,
+  },
+];
+const convenorNavItems = [
+  {
+    text: "Dashboard",
+    icon: <HomeOutlined />,
+  },
+  {
+    text: "Events",
+    icon: null,
+  },
+  {
+    text: "Create Event",
+    icon: <EventOutlined />,
+  },
+  {
+    text: "Past Events",
+    icon: <EventRepeatOutlined />,
+  },
+  {
+    text: "Committees",
+    icon: null,
+  },
+  {
+    text: "Committee Discreption",
+    icon: <VisibilityOutlined />,
+  },
+  {
+    text: "Management",
+    icon: null,
+  },
+  {
+    text: "Members",
+    icon: <VisibilityOutlined />,
+  },
+  {
+    text: "Add Members",
     icon: <ControlPointOutlined />,
   },
 ];
@@ -82,19 +120,26 @@ const Sidebar = ({
   isSidebarOpen,
   setIsSidebarOpen,
   isNonMobile,
+  user,
 }) => {
   const { pathname } = useLocation();
   const [active, setActive] = useState("");
   const navigate = useNavigate();
   const theme = useTheme();
+  const dispatch = useDispatch();
+  // const user = useSelector((state) => state.user);
 
   useEffect(() => {
     setActive(pathname.substring(1));
   }, [pathname]);
 
+  const handleLogout = () => {
+    dispatch(setLogout());
+    navigate("/admin");
+  };
   return (
     <Box component="nav">
-      {isSidebarOpen && (
+      {isSidebarOpen && user && (
         <Drawer
           open={isSidebarOpen}
           onClose={() => setIsSidebarOpen(false)}
@@ -111,7 +156,7 @@ const Sidebar = ({
             },
           }}
         >
-          <Box>
+          <Box position="relative">
             <Box m="1.5rem 2rem 2rem 3rem">
               <FlexBetween color={theme.palette.secondary.main}>
                 <Box display="flex" alignItems="center" gap="0.5rem">
@@ -127,56 +172,155 @@ const Sidebar = ({
               </FlexBetween>
             </Box>
             <List>
-              {navItems.map(({ text, icon }) => {
-                if (!icon) {
+              {user.role === "admin" &&
+                adminNavItems.map(({ text, icon }) => {
+                  if (!icon) {
+                    return (
+                      <Typography key={text} sx={{ m: "2.25rem 0 1rem 3rem" }}>
+                        {text}
+                      </Typography>
+                    );
+                  }
+                  const lcText = text.replaceAll(" ", "-").toLocaleLowerCase();
                   return (
-                    <Typography key={text} sx={{ m: "2.25rem 0 1rem 3rem" }}>
-                      {text}
-                    </Typography>
-                  );
-                }
-                const lcText = text.replaceAll(" ", "-").toLocaleLowerCase();
-                return (
-                  <ListItem key={text} disablePadding>
-                    <ListItemButton
-                      onClick={() => {
-                        navigate(`/${lcText}`);
-                        setActive(lcText);
-                        if (!isNonMobile) {
-                          setIsSidebarOpen(!isSidebarOpen);
-                        }
-                      }}
-                      sx={{
-                        backgroundColor:
-                          active === lcText
-                            ? theme.palette.secondary[300]
-                            : "transparent",
-                        color:
-                          active === lcText
-                            ? theme.palette.primary[600]
-                            : theme.palette.secondary[100],
-                      }}
-                    >
-                      <ListItemIcon
+                    <ListItem key={text} disablePadding>
+                      <ListItemButton
+                        onClick={() => {
+                          navigate(`/${lcText}`);
+                          setActive(lcText);
+                          if (!isNonMobile) {
+                            setIsSidebarOpen(!isSidebarOpen);
+                          }
+                        }}
                         sx={{
-                          ml: "2rem",
+                          backgroundColor:
+                            active === lcText
+                              ? theme.palette.secondary[300]
+                              : "transparent",
                           color:
                             active === lcText
                               ? theme.palette.primary[600]
-                              : theme.palette.secondary[200],
+                              : theme.palette.secondary[100],
                         }}
                       >
-                        {icon}
-                      </ListItemIcon>
-                      <ListItemText primary={text} />
-                      {active === lcText && (
-                        <ChevronRightOutlined sx={{ ml: "auto" }} />
-                      )}
-                    </ListItemButton>
-                  </ListItem>
-                );
-              })}
+                        <ListItemIcon
+                          sx={{
+                            ml: "1rem",
+                            color:
+                              active === lcText
+                                ? theme.palette.primary[600]
+                                : theme.palette.secondary[200],
+                          }}
+                        >
+                          {icon}
+                        </ListItemIcon>
+                        <ListItemText primary={text} />
+                        {active === lcText && (
+                          <ChevronRightOutlined sx={{ ml: "auto" }} />
+                        )}
+                      </ListItemButton>
+                    </ListItem>
+                  );
+                })}
+              {user.role === "convenor" &&
+                convenorNavItems.map(({ text, icon }) => {
+                  if (!icon) {
+                    return (
+                      <Typography key={text} sx={{ m: "2.25rem 0 1rem 3rem" }}>
+                        {text}
+                      </Typography>
+                    );
+                  }
+                  const lcText = text.replaceAll(" ", "-").toLocaleLowerCase();
+                  return (
+                    <ListItem key={text} disablePadding>
+                      <ListItemButton
+                        onClick={() => {
+                          navigate(`/${lcText}`);
+                          setActive(lcText);
+                          if (!isNonMobile) {
+                            setIsSidebarOpen(!isSidebarOpen);
+                          }
+                        }}
+                        sx={{
+                          backgroundColor:
+                            active === lcText
+                              ? theme.palette.secondary[300]
+                              : "transparent",
+                          color:
+                            active === lcText
+                              ? theme.palette.primary[600]
+                              : theme.palette.secondary[100],
+                        }}
+                      >
+                        <ListItemIcon
+                          sx={{
+                            ml: "1rem",
+                            color:
+                              active === lcText
+                                ? theme.palette.primary[600]
+                                : theme.palette.secondary[200],
+                          }}
+                        >
+                          {icon}
+                        </ListItemIcon>
+                        <ListItemText primary={text} />
+                        {active === lcText && (
+                          <ChevronRightOutlined sx={{ ml: "auto" }} />
+                        )}
+                      </ListItemButton>
+                    </ListItem>
+                  );
+                })}
             </List>
+          </Box>
+          <Box marginBottom="1rem">
+            <Divider />
+            <FlexBetween
+              textTransform="none"
+              gap="1.1rem"
+              m="1.5rem 2rem 0 1rem"
+            >
+              <Box
+                component="img"
+                alt="profile"
+                src={Profile}
+                height="40px"
+                width="40px"
+                borderRadius="50%"
+                sx={{ objectFit: "cover" }}
+              />
+              <Box textAlign="left">
+                <Typography
+                  fontWeight="bold"
+                  fontSize="0.9rem"
+                  sx={{ color: theme.palette.secondary[100] }}
+                >
+                  {user.name}
+                </Typography>
+                <Typography
+                  fontSize="0.8rem"
+                  sx={{ color: theme.palette.secondary[200] }}
+                >
+                  {user.role}
+                </Typography>
+              </Box>
+              <Tooltip
+                TransitionComponent={Zoom}
+                title="logout"
+                arrow
+                sx={{ fontSize: "bold" }}
+              >
+                <IconButton aria-label="logout" onClick={handleLogout}>
+                  <LogoutOutlinedIcon
+                    sx={{
+                      color: theme.palette.secondary[300],
+                      fontSize: "25px ",
+                    }}
+                  />
+                </IconButton>
+              </Tooltip>
+            </FlexBetween>
           </Box>
         </Drawer>
       )}
