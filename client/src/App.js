3c07e64bdbccc1594f42608e7127548ecde13d6d
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-03-27 00:17:24 +0530

    added protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,12 @@ import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { themeSettings } from "theme";
-import Dashboard from "scenes/dashboard/Dashboard";
-import Layout from "scenes/layout/Layout";
-import Home from "scenes/home/Home";
-function App() {
-    const mode = useSelector((state) => state.global.mode);
+import {Home,Dashboard, Layout, AddCommiittees, Login, AllCommittees, Committees,Convenors, AddConvenor, CreateEvent} from "./scenes"
+function App() { 
+    const mode = useSelector((state) => state.mode);
     const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+    const isAuth = Boolean(useSelector((state) => state.token));
+    const user = useSelector((state)=> state.user);
   return (
     <div className="app">
         <BrowserRouter>
@@ -17,9 +17,18 @@ function App() {
                 <CssBaseline/>
                 <Routes>
                     <Route path="/" element={<Home/>} />
-                    <Route element={<Layout/>} >
-                        <Route path="/admin" element={<Navigate to="/dashboard" replace />}  />
-                        <Route path="/dashboard" element={<Dashboard />} />    
+                    <Route path="/committees" element={<Committees/>} />
+                    <Route  path="/admin" element={!isAuth ? <Login /> : <Navigate to="/dashboard" />}  />
+                    <Route element={<Layout />} >
+                        {/* admin Routes */}
+                        <Route path="/dashboard" element={isAuth ? <Dashboard /> : <Navigate to="/admin" />}  />  
+                        <Route path="/add-committees" element={isAuth && user.role === "admin" ? <AddCommiittees /> : <Navigate to="/admin" />}  />
+                        <Route path="/view-committees" element={isAuth && user.role === "admin" ? <AllCommittees /> : <Navigate to="/admin" />}  />
+                        <Route path="/convenors" element={isAuth && user.role === "admin"  ? <Convenors /> : <Navigate to="/admin" />}  />
+                        <Route path="/add-convenors" element={isAuth && user.role === "admin" ? <AddConvenor /> : <Navigate to="/admin" />}  />
+                        {/* convenor routes */}
+                        <Route path="/create-event" element={isAuth && user.role === "convenor" ? <CreateEvent /> : <Navigate to="/admin" />}  />
+
                     </Route> 
                 </Routes>
             </ThemeProvider>
