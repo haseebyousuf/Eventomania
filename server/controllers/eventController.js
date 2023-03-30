af21df6a3ef279ab91a527b1359ea117f853c824
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-03-31 03:32:47 +0530

    refactored eventController

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -1,20 +1,21 @@
 import Event from "../models/Event.js";
 export const createEvent  = async (req, res) => {
   try {
-    // console.log(req.files);
-    const {banner, orderFile} = req.files;
+    //get files from req.files
+    const {banner, order} = req.files;
     const bannerName = banner[0].filename;
     const bannerPath = banner[0].path;
-    const orderFileName = orderFile[0].filename;
-    const orderFilePath = orderFile[0].path;
-
-    console.log(bannerName,bannerPath,orderFilePath);
+    const orderName = order[0].filename;
+    const orderPath = order[0].path;
+    //get rest of event details form req.body
     const{ name, venue, startDate, endDate, description, recomendedAudiance, committee, createdBy} = req.body;
+    //parse committee and creator details
     const parsedCommittee = JSON.parse(committee);
     const parsedCreator = JSON.parse(createdBy);
-
-    const newEvent = new Event({name, venue, startDate, endDate, description, recomendedAudiance,bannerName,bannerPath, orderFileName,orderFilePath, committee:parsedCommittee, createdBy: parsedCreator});
+    //save data to db
+    const newEvent = new Event({name, venue, startDate, endDate, description, recomendedAudiance,bannerName,bannerPath, orderName,orderPath, committee:parsedCommittee, createdBy: parsedCreator});
     const savedEvent = await newEvent.save();
+    //send success response
     res.status(201).json(savedEvent);
   } catch (error) {
     res.status(500).json({ error: error.message });
