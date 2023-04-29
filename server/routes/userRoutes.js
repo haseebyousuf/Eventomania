fb749c40c04a0d7aeebc492d786218c036214653
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-04-29 09:32:30 +0530

    added routes for student registration

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,8 @@
 import express from "express";
+import { registerStudent } from "../controllers/userController.js";
 
 const router = express.Router();
+router.post("/registerStudent", registerStudent);
 
 
 export default router;
