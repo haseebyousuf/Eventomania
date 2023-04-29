438d29bde089efffaaf8a1ce4df65680efdc2f9d
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-04-29 10:08:58 +0530

    created registerFaculty controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -29,4 +29,32 @@ export const registerStudent = async (req, res) => {
       res.status(500).json({ error: error.message });
   }
 };
+export const registerFaculty = async (req, res) => {
+  try {
+      const { name, employeeId, designation, department, event, type } =
+          req.body;
+          //check if faculty already registered for the given event
+      const user = await User.find({
+        employeeId: employeeId,
+          event: { $elemMatch: { name: event.name } },
+      });
+      //return error if faculty already registered
+      if (user.length > 0) {
+          return res.status(400).json({ msg: "You Have Already Registered!" });
+      }
+      //create new user
+      const newUser = new User({
+          name,
+          employeeId,
+          designation,
+          department,
+          event,
+          type,
+      });
+      const savedUser = await newUser.save();
+      res.status(201).json(savedUser);
+  } catch (error) {
+      res.status(500).json({ error: error.message });
+  }
+};
 
