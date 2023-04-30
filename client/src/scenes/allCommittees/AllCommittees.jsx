227fcbe8143560d462c5c23142deef299becfb5b
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-04-30 22:39:00 +0530

    refactored

--- client/src/scenes/allCommittees/AllCommittees.jsx
+++ client/src/scenes/allCommittees/AllCommittees.jsx
@@ -21,123 +21,102 @@ const AllCommittees = () => {
     getCommittees();
   }, []);
   const columns = [
-    {
-      field: "_id",
-      headerName: "ID",
-      minWidth: 200,
-      // flex: 1,
-    },
-    {
-      field: "name",
-      headerName: "Name",
-      minWidth: 100,
-      // flex: 1,
-    },
-    {
-      field: "description",
-      headerName: "Description",
-      // flex: 1,
-      minWidth: 270,
-    },
-    {
-      field: "convenorName",
-      headerName: "Convenor",
-      minWidth: 200,
-      // flex: 1,
-    },
-    {
-      field: "createdAt",
-      headerName: "Created At",
-      minWidth: 200,
-      renderCell: (params) => {
-        return dayjs(params.row.createdAt).format("DD-MM-YYYY");
+      {
+          field: "name",
+          headerName: "Committee Name",
+          minWidth: 200,
       },
-    },
-    {
-      field: "members",
-      headerName: "Members",
-      minWidth: 100,
+      {
+          field: "description",
+          headerName: "Description",
+          minWidth: 270,
+      },
+      {
+          field: "convenorName",
+          headerName: "Convenor",
+          minWidth: 200,
+      },
+      {
+          field: "createdAt",
+          headerName: "Created At",
+          minWidth: 200,
+          renderCell: (params) => {
+              return dayjs(params.row.createdAt).format("DD-MM-YYYY");
+          },
+      },
+      {
+          field: "members",
+          headerName: "Members",
+          minWidth: 100,
 
-      // flex: 0.5,
-      renderCell: (params) => {
-        return params.value.length;
+          renderCell: (params) => {
+              return params.row.members.length;
+          },
       },
-    },
-    {
-      field: "events",
-      headerName: "No. of Events",
-      minWidth: 100,
-
-      // flex: 0.5,
-      renderCell: (params) => {
-        return params.value.length;
-      },
-    },
   ];
 
   return (
-    <Box m="1rem 2.5rem">
-      <Box
-        flexDirection="column"
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Typography
-          fontSize="1.5rem"
-          textDecoration="underline"
-          fontWeight="bold"
-          // p="0.5rem 0 0 0"
-          color={theme.palette.secondary.main}
-        >
-          COMMITTEES
-        </Typography>
-        <Typography
-          fontSize="1rem"
-          textDecoration="underline"
-          fontWeight="bold"
-          color={theme.palette.secondary.main}
-        >
-          List of All Committees
-        </Typography>
+      <Box m="1rem 2.5rem">
+          <Box
+              flexDirection="column"
+              display="flex"
+              justifyContent="center"
+              alignItems="flex-start"
+          >
+              <Typography
+                  fontSize="1.5rem"
+                  textDecoration="underline"
+                  fontWeight="bold"
+                  color={theme.palette.secondary.main}
+              >
+                  COMMITTEES
+              </Typography>
+              <Typography
+                  fontSize="1rem"
+                  textDecoration="underline"
+                  fontWeight="bold"
+                  color={theme.palette.secondary.main}
+              >
+                  List of All Committees
+              </Typography>
+          </Box>
+          <Box
+              mt="20px"
+              pb="20px"
+              height="75vh"
+              sx={{
+                  "& .MuiDataGrid-root": {
+                      border: "none",
+                  },
+                  "& .MuiDataGrid-cell": {
+                      borderBottom: "none",
+                  },
+                  "& .MuiDataGrid-columnHeaders": {
+                      backgroundColor: theme.palette.background.alt,
+                      color: theme.palette.secondary[100],
+                      borderBottom: "none",
+                  },
+                  "& .MuiDataGrid-virtualScroller": {
+                      backgroundColor: theme.palette.primary.light,
+                  },
+                  "& .MuiDataGrid-footerContainer": {
+                      backgroundColor: theme.palette.background.alt,
+                      color: theme.palette.secondary[100],
+                      borderTop: "none",
+                  },
+                  "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+                      color: `${theme.palette.secondary[200]} !important`,
+                  },
+              }}
+          >
+              <DataGrid
+                  loading={data.isLoading || !data}
+                  getRowId={(row) => row._id}
+                  rows={data.committees || []}
+                  columns={columns}
+              />
+          </Box>
       </Box>
-      <Box
-        mt="20px"
-        pb="20px"
-        height="75vh"
-        sx={{
-          "& .MuiDataGrid-root": {
-            border: "none",
-          },
-          "& .MuiDataGrid-cell": {
-            borderBottom: "none",
-          },
-          "& .MuiDataGrid-columnHeaders": {
-            backgroundColor: theme.palette.background.alt,
-            color: theme.palette.secondary[100],
-            borderBottom: "none",
-          },
-          "& .MuiDataGrid-virtualScroller": {
-            backgroundColor: theme.palette.primary.light,
-          },
-          "& .MuiDataGrid-footerContainer": {
-            backgroundColor: theme.palette.background.alt,
-            color: theme.palette.secondary[100],
-            borderTop: "none",
-          },
-          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
-            color: `${theme.palette.secondary[200]} !important`,
-          },
-        }}
-      >
-        <DataGrid
-          loading={data.isLoading || !data}
-          getRowId={(row) => row._id}
-          rows={data.committees || []}
-          columns={columns}
-        />
-      </Box>
-    </Box>
   );
 };
 
