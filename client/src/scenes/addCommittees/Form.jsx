264aff7b8827dda39a810ef810adc2ffa9b7fff8
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-04-30 23:10:31 +0530

    added animations and snackbar

--- client/src/scenes/addCommittees/Form.jsx
+++ client/src/scenes/addCommittees/Form.jsx
@@ -1,151 +1,166 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import {
-  Box,
-  Card,
-  CardContent,
-  TextField,
-  Button,
-  Typography,
-  useTheme,
-  CardActions,
-  Alert,
-  AlertTitle,
+    Box,
+    Card,
+    CardContent,
+    TextField,
+    Button,
+    CardActions,
+    Snackbar,
+    Alert,
+    Slide,
 } from "@mui/material";
 import { Formik } from "formik";
 import * as yup from "yup";
-import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { FlexBetween } from "../../components/FlexBetween";
-
-const addCommitteeScheema = yup.object().shape({
-  name: yup.string().required("*Name Required"),
-  description: yup.string().required("required"),
+import { motion } from "framer-motion";
+import { useSelector } from "react-redux";
+import { useTheme } from "@emotion/react";
+const inputs = [
+    { id: 1, label: "Committee Name", name: "name" },
+    { id: 2, label: "Description", name: "description" },
+];
+const addCommitteeSchema = yup.object().shape({
+    name: yup.string().required("*Name is Required"),
+    description: yup.string().required("Description is required"),
 });
 
 const initialValuesCommittee = {
-  name: "",
-  description: "",
+    name: "",
+    description: "",
 };
 const Form = () => {
-  const theme = useTheme();
+    const mode = useSelector((state) => state.mode);
+    const theme = useTheme();
 
-  const handleFormSubmit = async (values, onSubmitProps) => {
-    try {
-      const savedCommitteeResponse = await axios({
-        method: "post",
-        url: "http://localhost:5001/committee/add-committee",
-        headers: { "Content-Type": "application/json" },
-        data: JSON.stringify(values),
-      });
-      const savedCommittee = await savedCommitteeResponse.data;
-      onSubmitProps.resetForm();
-      if (savedCommittee) {
-        alert("Added Successfully!");
-        // <Alert variant="outlined" severity="success">
-        //   <AlertTitle>Added</AlertTitle>
-        //   <strong>{savedCommittee.name}</strong> added successfully!
-        // </Alert>;
-      }
-    } catch (error) {
-      alert("error");
-      // <Alert variant="outlined" severity="error">
-      //   <AlertTitle>Error</AlertTitle>
-      //   Failed to connect to server
-      // </Alert>;
-    }
-  };
-  return (
-    <Formik
-      onSubmit={handleFormSubmit}
-      initialValues={initialValuesCommittee}
-      validationSchema={addCommitteeScheema}
-    >
-      {({
-        values,
-        errors,
-        touched,
-        handleBlur,
-        handleChange,
-        handleSubmit,
-        setFieldValue,
-        resetForm,
-      }) => (
-        <form onSubmit={handleSubmit}>
-          <Box>
-            <Card
-              sx={{
-                backgroundColor: "transparent",
-              }}
-            >
-              <CardContent>
-                <Box display="flex" justifyContent="center" alignItems="center">
-                  <Typography
-                    fontSize="2rem"
-                    textDecoration="underline"
-                    fontWeight="bold"
-                    p="1rem"
-                    color={theme.palette.secondary.main}
-                  >
-                    ADD COMMITTEE
-                  </Typography>
-                </Box>
-                <TextField
-                  id="name"
-                  autoComplete="off"
-                  color="secondary"
-                  label="Committe Name"
-                  value={values.name}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  helperText={touched.name ? errors.name : ""}
-                  error={touched.name && Boolean(errors.name)}
-                  margin="dense"
-                  variant="outlined"
-                  fullWidth
-                />
-                <TextField
-                  notched="true"
-                  autoComplete="off"
-                  color="secondary"
-                  id="description"
-                  label="Description"
-                  value={values.description}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  helperText={touched.description ? errors.description : ""}
-                  error={touched.description && Boolean(errors.description)}
-                  margin="dense"
-                  variant="outlined"
-                  fullWidth
-                />
-              </CardContent>
-              <CardActions
-                display="flex"
-                sx={{
-                  marginBottom: "1rem",
-                  justifyContent: "center",
-                }}
-              >
-                <Button
-                  variant="contained"
-                  type="submit"
-                  sx={{
-                    color: "black",
-                    fontWeight: "bold",
-                  }}
-                  size="large"
-                  color="secondary"
-                >
-                  Add Committee
-                </Button>
-              </CardActions>
-            </Card>
-          </Box>
-        </form>
-      )}
-    </Formik>
-  );
+    const [open, setOpen] = useState(false);
+    const [message, setMessage] = useState(null);
+
+    const handleFormSubmit = async (values, onSubmitProps) => {
+        try {
+            const savedCommitteeResponse = await axios({
+                method: "post",
+
+                url: `${process.env.REACT_APP_BASE_URL}/committee/add-committee`,
+                headers: { "Content-Type": "application/json" },
+                data: JSON.stringify(values),
+            });
+            const savedCommittee = await savedCommitteeResponse.data;
+            onSubmitProps.resetForm();
+            if (savedCommittee) {
+                setMessage("Registered Successfully!");
+                setOpen(true);
+                setTimeout(() => {
+                    setOpen(false);
+                }, 3000);
+            }
+        } catch (error) {
+            alert("There is some error! Please Try Again.");
+        }
+    };
+    //transition for snackbar
+    const SlideTransition = (props) => {
+        return <Slide {...props} direction="down" />;
+    };
+    return (
+        <Formik
+            onSubmit={handleFormSubmit}
+            initialValues={initialValuesCommittee}
+            validationSchema={addCommitteeSchema}
+        >
+            {({
+                values,
+                errors,
+                touched,
+                handleBlur,
+                handleChange,
+                handleSubmit,
+            }) => (
+                <form onSubmit={handleSubmit}>
+                    <Snackbar
+                        sx={{ position: "absolute" }}
+                        open={open}
+                        autoHideDuration={6000}
+                        TransitionComponent={SlideTransition}
+                        anchorOrigin={{
+                            vertical: "top",
+                            horizontal: "center",
+                        }}
+                    >
+                        <Alert variant="filled" severity="success">
+                            {message}
+                        </Alert>
+                    </Snackbar>
+                    <Box>
+                        <Card
+                            sx={{
+                                backgroundColor:
+                                    mode === "dark"
+                                        ? "transparent"
+                                        : theme.palette.background.alt,
+                            }}
+                        >
+                            <CardContent>
+                                {inputs.map((input, index) => (
+                                    <TextField
+                                        component={motion.div}
+                                        initial={{ y: 20, opacity: 0 }}
+                                        animate={{ y: 0, opacity: 1 }}
+                                        transition={{ delay: 0.13 * input.id }}
+                                        exit={{ y: 20, opacity: 0 }}
+                                        key={input.id}
+                                        id={input.name}
+                                        autoComplete="off"
+                                        color="secondary"
+                                        label={input.label}
+                                        value={values[input.name]}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        multiline={
+                                            input.id === 2 ? true : false
+                                        }
+                                        minRows={3}
+                                        helperText={
+                                            touched[input.name]
+                                                ? errors[input.name]
+                                                : ""
+                                        }
+                                        error={
+                                            touched[input.name] &&
+                                            Boolean(errors[input.name])
+                                        }
+                                        margin="dense"
+                                        variant="outlined"
+                                        fullWidth
+                                    />
+                                ))}
+                            </CardContent>
+                            <CardActions
+                                display="flex"
+                                sx={{
+                                    marginBottom: "1rem",
+                                    justifyContent: "center",
+                                }}
+                            >
+                                <Button
+                                    variant="contained"
+                                    type="submit"
+                                    sx={{
+                                        color: "black",
+                                        fontWeight: "bold",
+                                    }}
+                                    size="large"
+                                    color="secondary"
+                                >
+                                    Add Committee
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </Box>
+                </form>
+            )}
+        </Formik>
+    );
 };
 
 export default Form;
