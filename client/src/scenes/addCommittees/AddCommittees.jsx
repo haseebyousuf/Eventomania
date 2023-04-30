825896491d5fa602f22b5d7e886d7899cb8b5e66
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-04-30 23:10:03 +0530

    moved page title outside card

--- client/src/scenes/addCommittees/AddCommittees.jsx
+++ client/src/scenes/addCommittees/AddCommittees.jsx
@@ -8,7 +8,35 @@ const AddCommittees = () => {
     const theme = useTheme();
     return (
         <Box>
-            <Box width={isNonMobile ? "80%" : "90%"} m="2rem auto">
+            <Box
+                width={isNonMobile ? "80%" : "90%"}
+                m="2rem auto"
+                position="relative"
+            >
+                <Box
+                    marginBottom="1rem"
+                    flexDirection="column"
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="flex-start"
+                >
+                    <Typography
+                        fontSize="1.5rem"
+                        textDecoration="underline"
+                        fontWeight="bold"
+                        color={theme.palette.secondary.main}
+                    >
+                        ADD COMMITTEE
+                    </Typography>
+                    <Typography
+                        fontSize="1rem"
+                        textDecoration="underline"
+                        fontWeight="bold"
+                        color={theme.palette.secondary.main}
+                    >
+                        Add New Committee Details
+                    </Typography>
+                </Box>
                 <Form />
             </Box>
         </Box>
