f0193129aaada0ebd9c6db057af2462d74b37fd5
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-04-30 08:33:07 +0530

    renamed file

---
+++ client/src/scenes/addCommittees/AddCommittees.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
+
+import Form from "./Form";
+
+const AddCommittees = () => {
+    const isNonMobile = useMediaQuery("(min-width: 600px)");
+    const theme = useTheme();
+    return (
+        <Box>
+            <Box width={isNonMobile ? "80%" : "90%"} m="2rem auto">
+                <Form />
+            </Box>
+        </Box>
+    );
+};
+
+export default AddCommittees;
