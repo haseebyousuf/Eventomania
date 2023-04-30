30401bbb950437cfee556d0454342777aa317dff
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-04-30 08:33:41 +0530

    modified url paths

--- client/src/components/HomeNavbar.jsx
+++ client/src/components/HomeNavbar.jsx
@@ -60,178 +60,184 @@ const Navbar = () => {
   }, [isNonMobile]);
 
   return (
-    <AppBar
-      sx={{
-        position: "sticky",
-        backgroundColor:
-          mode === "dark" ? "#2d3355" : theme.palette.background.alt,
-        boxShadow: "0 2px 0px rgba(0 0 0 / 0.1)",
-        // borderBottom: "2px",
-        // BorderBottomColor: "red",
-      }}
-    >
-      <Toolbar
-        sx={{
-          justifyContent: "space-between",
-          margin: isNonMobile ? "auto 5rem auto 5rem" : "auto 2rem auto 2rem",
-          // padding: "auto 0rem auto 0rem",
-          paddingLeft: "0rem !important",
-          paddingRight: "0rem !important",
-        }}
+      <AppBar
+          sx={{
+              position: "sticky",
+              backgroundColor:
+                  mode === "dark" ? "#2d3355" : theme.palette.background.alt,
+              boxShadow: "0 2px 0px rgba(0 0 0 / 0.1)",
+              // borderBottom: "2px",
+              // BorderBottomColor: "red",
+          }}
       >
-        {/* LEFT SIDE  */}
-        <FlexBetween>
-          <Box
-            color={theme.palette.secondary.main}
-            display="flex"
-            alignItems="center"
-            gap="0.5rem"
-          >
-            <Typography
-              sx={{ fontSize: "1.5rem" }}
-              varient="h1"
-              fontWeight="bold"
-            >
-              EVENTOMANIA
-            </Typography>
-          </Box>
-        </FlexBetween>
-        {/* RIGHT SIDE */}
-        {isNonMobile ? (
-          <FlexBetween gap="1.5rem">
-            <Tabs
-              value={value}
-              onChange={(e, value) => setValue(value)}
-              indicatorColor="secondary"
-              textColor="secondary"
-            >
-              {navItems.map(({ text, link }) => {
-                return (
-                  <Tab
-                    onClick={() => {
-                      navigate(link);
-                    }}
-                    label={text}
-                    key={text}
-                  />
-                );
-              })}
-            </Tabs>
-            <Button
-              variant="contained"
+          <Toolbar
               sx={{
-                borderRadius: "100px",
-                color: "black",
-                fontWeight: "600",
+                  justifyContent: "space-between",
+                  margin: isNonMobile
+                      ? "auto 5rem auto 5rem"
+                      : "auto 2rem auto 2rem",
+                  // padding: "auto 0rem auto 0rem",
+                  paddingLeft: "0rem !important",
+                  paddingRight: "0rem !important",
               }}
-              onClick={() => navigate("/admin")}
-              color="secondary"
-            >
-              <AccountCircleOutlined />
-            </Button>
-            <IconButton onClick={() => dispatch(setMode())}>
-              {theme.palette.mode === "dark" ? (
-                <DarkModeOutlined sx={{ fontSize: "25px" }} />
-              ) : (
-                <LightModeOutlined sx={{ fontSize: "25px" }} />
-              )}
-            </IconButton>
-          </FlexBetween>
-        ) : (
-          <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
-            <MenuIcon />
-          </IconButton>
-        )}
-      </Toolbar>
-      <Drawer
-        open={isSidebarOpen}
-        onClose={() => setIsSidebarOpen(false)}
-        variant="persistent"
-        anchor="right"
-        sx={{
-          width: "250px",
-          "& .MuiDrawer-paper": {
-            color: theme.palette.secondary[200],
-            backgroundColor: theme.palette.background.alt,
-            boxSizing: "border-box",
-            borderWidth: isNonMobile ? 0 : "2px",
-            width: "250px",
-          },
-        }}
-      >
-        <Box>
-          <Box m="1.5rem 4rem 2rem 1rem">
-            <FlexBetween color={theme.palette.secondary.main}>
-              {!isNonMobile && (
-                <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
-                  <ChevronRight />
-                </IconButton>
-              )}
-              <Box display="flex" alignItems="center" gap="0.5rem">
-                <Typography varient="h4" fontWeight="bold">
-                  EVENTOMANIA
-                </Typography>
-              </Box>
-            </FlexBetween>
-          </Box>
-          <List>
-            {navItems.map(({ text }) => {
-              return (
-                <ListItem key={text} disablePadding>
-                  <ListItemButton
-                    onClick={() => {
-                      // navigate(`/${lcText}`);
-                      // setActive(lcText);
-                      if (!isNonMobile) {
-                        setIsSidebarOpen(!isSidebarOpen);
-                      }
-                    }}
-                    // sx={{
-                    //   backgroundColor:
-                    //     active === lcText
-                    //       ? theme.palette.secondary[300]
-                    //       : "transparent",
-                    //   color:
-                    //     active === lcText
-                    //       ? theme.palette.primary[600]
-                    //       : theme.palette.secondary[100],
-                    // }}
+          >
+              {/* LEFT SIDE  */}
+              <FlexBetween>
+                  <Box
+                      color={theme.palette.secondary.main}
+                      display="flex"
+                      alignItems="center"
+                      gap="0.5rem"
                   >
-                    <ListItemText primary={text} />
-                    {/* {active === lcText && (
+                      <Typography
+                          sx={{ fontSize: "1.5rem" }}
+                          varient="h1"
+                          fontWeight="bold"
+                      >
+                          EVENTOMANIA
+                      </Typography>
+                  </Box>
+              </FlexBetween>
+              {/* RIGHT SIDE */}
+              {isNonMobile ? (
+                  <FlexBetween gap="1.5rem">
+                      <Tabs
+                          value={value}
+                          onChange={(e, value) => setValue(value)}
+                          indicatorColor="secondary"
+                          textColor="secondary"
+                      >
+                          {navItems.map(({ text, link }) => {
+                              return (
+                                  <Tab
+                                      onClick={() => {
+                                          navigate(link);
+                                      }}
+                                      label={text}
+                                      key={text}
+                                  />
+                              );
+                          })}
+                      </Tabs>
+                      <Button
+                          variant="contained"
+                          sx={{
+                              borderRadius: "100px",
+                              color: "black",
+                              fontWeight: "600",
+                          }}
+                          onClick={() => navigate("/Login")}
+                          color="secondary"
+                      >
+                          <AccountCircleOutlined />
+                      </Button>
+                      <IconButton onClick={() => dispatch(setMode())}>
+                          {theme.palette.mode === "dark" ? (
+                              <DarkModeOutlined sx={{ fontSize: "25px" }} />
+                          ) : (
+                              <LightModeOutlined sx={{ fontSize: "25px" }} />
+                          )}
+                      </IconButton>
+                  </FlexBetween>
+              ) : (
+                  <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+                      <MenuIcon />
+                  </IconButton>
+              )}
+          </Toolbar>
+          <Drawer
+              open={isSidebarOpen}
+              onClose={() => setIsSidebarOpen(false)}
+              variant="persistent"
+              anchor="right"
+              sx={{
+                  width: "250px",
+                  "& .MuiDrawer-paper": {
+                      color: theme.palette.secondary[200],
+                      backgroundColor: theme.palette.background.alt,
+                      boxSizing: "border-box",
+                      borderWidth: isNonMobile ? 0 : "2px",
+                      width: "250px",
+                  },
+              }}
+          >
+              <Box>
+                  <Box m="1.5rem 4rem 2rem 1rem">
+                      <FlexBetween color={theme.palette.secondary.main}>
+                          {!isNonMobile && (
+                              <IconButton
+                                  onClick={() =>
+                                      setIsSidebarOpen(!isSidebarOpen)
+                                  }
+                              >
+                                  <ChevronRight />
+                              </IconButton>
+                          )}
+                          <Box display="flex" alignItems="center" gap="0.5rem">
+                              <Typography varient="h4" fontWeight="bold">
+                                  EVENTOMANIA
+                              </Typography>
+                          </Box>
+                      </FlexBetween>
+                  </Box>
+                  <List>
+                      {navItems.map(({ text }) => {
+                          return (
+                              <ListItem key={text} disablePadding>
+                                  <ListItemButton
+                                      onClick={() => {
+                                          // navigate(`/${lcText}`);
+                                          // setActive(lcText);
+                                          if (!isNonMobile) {
+                                              setIsSidebarOpen(!isSidebarOpen);
+                                          }
+                                      }}
+                                      // sx={{
+                                      //   backgroundColor:
+                                      //     active === lcText
+                                      //       ? theme.palette.secondary[300]
+                                      //       : "transparent",
+                                      //   color:
+                                      //     active === lcText
+                                      //       ? theme.palette.primary[600]
+                                      //       : theme.palette.secondary[100],
+                                      // }}
+                                  >
+                                      <ListItemText primary={text} />
+                                      {/* {active === lcText && (
                         <ChevronRightOutlined sx={{ ml: "auto" }} />
                       )} */}
-                  </ListItemButton>
-                </ListItem>
-              );
-            })}
-          </List>
-        </Box>
-        <Button
-          variant="contained"
-          sx={{
-            // borderRadius: "100px",
-            margin: "1rem",
-            color: "black",
-            fontWeight: "bold",
-          }}
-          onClick={() => navigate("/admin")}
-          color="secondary"
-        >
-          Sign IN
-        </Button>
-        <IconButton
-          sx={{ position: "fixed", bottom: 5 }}
-          onClick={() => dispatch(setMode())}
-        >
-          {theme.palette.mode === "dark" ? (
-            <DarkModeOutlined sx={{ fontSize: "25px" }} />
-          ) : (
-            <LightModeOutlined sx={{ fontSize: "25px" }} />
-          )}
-        </IconButton>
-      </Drawer>
-    </AppBar>
+                                  </ListItemButton>
+                              </ListItem>
+                          );
+                      })}
+                  </List>
+              </Box>
+              <Button
+                  variant="contained"
+                  sx={{
+                      // borderRadius: "100px",
+                      margin: "1rem",
+                      color: "black",
+                      fontWeight: "bold",
+                  }}
+                  onClick={() => navigate("/Login")}
+                  color="secondary"
+              >
+                  Sign IN
+              </Button>
+              <IconButton
+                  sx={{ position: "fixed", bottom: 5 }}
+                  onClick={() => dispatch(setMode())}
+              >
+                  {theme.palette.mode === "dark" ? (
+                      <DarkModeOutlined sx={{ fontSize: "25px" }} />
+                  ) : (
+                      <LightModeOutlined sx={{ fontSize: "25px" }} />
+                  )}
+              </IconButton>
+          </Drawer>
+      </AppBar>
   );
 };
 
