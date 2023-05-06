b1d34b96ba195d294ff4f60c44714d81b649d040
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-05-06 20:20:36 +0530

    added header component for page title

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,27 @@
+import { Typography, Box, useTheme } from "@mui/material";
+import React from "react";
+
+const Header = ({ title, subtitle }) => {
+    const theme = useTheme();
+    return (
+        <Box>
+            <Typography
+                variant="h2"
+                color={theme.palette.secondary[100]}
+                fontWeight="bold"
+                sx={{ mb: "5px" }}
+            >
+                {title}
+            </Typography>
+            <Typography
+                sx={{ mb: "5px" }}
+                variant="h5"
+                color={theme.palette.secondary[300]}
+            >
+                {subtitle}
+            </Typography>
+        </Box>
+    );
+};
+
+export default Header;
