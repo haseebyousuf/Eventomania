4eafac67065c70967e8bdab5b0a109a2aca58661
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-05-07 19:24:11 +0530

    refactor: added Header component and transition to page

--- client/src/scenes/addCommittees/AddCommittees.jsx
+++ client/src/scenes/addCommittees/AddCommittees.jsx
@@ -3,11 +3,19 @@ import { Box } from "@mui/material";
 
 import Form from "./Form";
 import Header from "components/Header";
+import { motion } from "framer-motion";
 
 const AddCommittees = () => {
     return (
         <Box>
-            <Box m="1rem 2.5rem" position="relative">
+            <Box
+                m="1rem 2.5rem"
+                position="relative"
+                component={motion.div}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 0.1, ease: "easeInOut" }}
+            >
                 <Header
                     title="ADD COMMITTEE"
                     subtitle="Add New Committee Details."
