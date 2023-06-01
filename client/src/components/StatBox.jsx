17cfc884b380279fe4565ad4a3ec12478c614f3a
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-06-01 19:30:36 +0530

    feat: added statsBox to be displayed in dashboard

---
+++ client/src/components/StatBox.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Box, Typography, useTheme } from "@mui/material";
+import FlexBetween from "./FlexBetween";
+
+const StatBox = ({ title, value, icon, description }) => {
+    const theme = useTheme();
+    return (
+        <Box
+            gridColumn="span 2"
+            gridRow="span 1"
+            display="flex"
+            flexDirection="column"
+            justifyContent="space-between"
+            p="1.25rem 1rem"
+            flex="1 1 100%"
+            backgroundColor={theme.palette.background.alt}
+            borderRadius="0.55rem"
+        >
+            <FlexBetween>
+                <Typography
+                    variant="h6"
+                    sx={{ color: theme.palette.secondary[100] }}
+                >
+                    {title}
+                </Typography>
+                {icon}
+            </FlexBetween>
+
+            <Typography
+                variant="h3"
+                fontWeight="600"
+                sx={{ color: theme.palette.secondary[200] }}
+            >
+                {value}
+            </Typography>
+            <Box
+                sx={{
+                    display: "flex",
+                    flexDirection: "row-reverse",
+                }}
+            >
+                <Typography>{description}</Typography>
+            </Box>
+        </Box>
+    );
+};
+
+export default StatBox;
