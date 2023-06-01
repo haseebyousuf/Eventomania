dd5e802c824cc9e0788ae68926cee64bf7c9b182
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-06-01 19:34:27 +0530

    feat: added eventsPerMonth controller

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -141,3 +141,50 @@ export const eventsPerMonth = async (req, res) => {
     res.status(500).json({ error: 'An error occurred while fetching events per month.' });
   }
 };
+
+
+export const eventsPerMonthByCommittee = async (req, res) => {
+  try {
+    const currentYear = moment().year(); // Get the current year
+    const currentMonth = moment().month(); // Get the current month (0-11)
+    const committeeId = req.body.committeeId; // Get the committee ID from the request body
+
+    const events = await Event.find({
+      isApproved: true,
+      'committee.id': committeeId
+    }).exec();
+
+    // Create an object to store the count of events per month
+    const eventsCountByMonth = {};
+    for (const event of events) {
+      const startDate = moment(event.startDate);
+      if (
+        startDate.year() === currentYear &&
+        startDate.month() <= currentMonth
+      ) {
+        const monthName = startDate.format('MMM');
+        if (eventsCountByMonth[monthName]) {
+          eventsCountByMonth[monthName]++;
+        } else {
+          eventsCountByMonth[monthName] = 1;
+        }
+      }
+    }
+
+    // Generate an array of month names up to the current month
+    const allMonths = moment.monthsShort().slice(0, currentMonth + 1);
+
+    // Format data for Nivo line chart
+    const formattedData = allMonths.map(month => ({
+      x: month,
+      y: eventsCountByMonth[month] || 0
+    }));
+
+    res.status(201).json(formattedData);
+  } catch (error) {
+    console.log('Error:', error); // Log the error for debugging
+    res
+      .status(500)
+      .json({ error: 'An error occurred while fetching events per month for the committee.' });
+  }
+};
