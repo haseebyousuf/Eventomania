934cf5b0516659e76dfefa1d848937816449e4ce
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-06-01 19:32:44 +0530

    refactor: removed unnecessary imports

--- client/src/scenes/viewCommittee/ViewCommittee.jsx
+++ client/src/scenes/viewCommittee/ViewCommittee.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
-import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
+import React from "react";
+// import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
 
 const ViewCommittee = () => {
-  return <div>ViewCommittee</div>;
+    return <div>ViewCommittee</div>;
 };
 
 export default ViewCommittee;
