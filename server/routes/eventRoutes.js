94feb77b0f2780ab2ab768dd307aae11b46690be
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-06-03 11:28:32 +0530

    added delete route

--- server/routes/eventRoutes.js
+++ server/routes/eventRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {getUnApprovedEvents, eventsPerMonth, getEvents,togglePublish,approveEvent, getPublishedEvents, getApprovedEvents} from "../controllers/eventController.js"
+import {getUnApprovedEvents, deleteEvent, getEvents,togglePublish,approveEvent, getPublishedEvents, getApprovedEvents} from "../controllers/eventController.js"
 
 const router = express.Router();
 
@@ -9,6 +9,7 @@ router.post("/approveEvent", approveEvent)
 router.get("/getPublishedEvents", getPublishedEvents)
 router.get("/getEvents", getEvents);
 router.post("/togglePublish", togglePublish)
-router.get("/eventsPerMonth", eventsPerMonth)
+router.post("/deleteEvent", deleteEvent)
+// router.get("/eventsPerMonth", eventsPerMonth)
 
 export default router;
