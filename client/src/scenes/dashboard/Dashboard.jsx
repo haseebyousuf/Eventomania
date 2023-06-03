3d4e3d9cc17fda4fa1cc202113a96a6d837c30d6
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-06-03 11:20:59 +0530

    fx: changed description and title of cards

--- client/src/scenes/dashboard/Dashboard.jsx
+++ client/src/scenes/dashboard/Dashboard.jsx
@@ -31,7 +31,6 @@ const Dashboard = () => {
         };
         getStats();
     }, []);
-
     const columns = [
         {
             field: "name",
@@ -70,145 +69,158 @@ const Dashboard = () => {
     ];
 
     return (
-        <Box m="1.5rem 2.5rem">
-            <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
-
-            <Box
-                mt="20px"
-                display="grid"
-                gridTemplateColumns="repeat(12, 1fr)"
-                gridAutoRows="160px"
-                gap="20px"
-                sx={{
-                    "& > div": {
-                        gridColumn: isNonMediumScreens ? undefined : "span 12",
-                    },
-                }}
-            >
-                {/* ROW 1 */}
-                <StatBox
-                    title="Total Events"
-                    value={data && data.approvedEventsCount}
-                    description="This Year"
-                    icon={
-                        <EventIcon
-                            sx={{
-                                color: theme.palette.secondary[300],
-                                fontSize: "26px",
-                            }}
-                        />
-                    }
-                />
-                <StatBox
-                    title="Pending"
-                    value={data && data.unapprovedEventsCount}
-                    description="Unapproved"
-                    icon={
-                        <PendingActionsIcon
-                            sx={{
-                                color: theme.palette.secondary[300],
-                                fontSize: "26px",
-                            }}
-                        />
-                    }
-                />
-                <Box
-                    gridColumn="span 8"
-                    gridRow="span 2"
-                    backgroundColor={theme.palette.background.alt}
-                    p="1rem"
-                    borderRadius="0.55rem"
-                >
-                    <OverallStats />
-                </Box>
-                <StatBox
-                    title="Members"
-                    value={data && data.adminsCount}
-                    description="Admins"
-                    icon={
-                        <ManageAccountsIcon
-                            sx={{
-                                color: theme.palette.secondary[300],
-                                fontSize: "26px",
-                            }}
-                        />
-                    }
-                />
-                <StatBox
-                    title="Committees"
-                    value={data && data.committeesCount}
-                    description="Total"
-                    icon={
-                        <Diversity3Outlined
-                            sx={{
-                                color: theme.palette.secondary[300],
-                                fontSize: "26px",
-                            }}
-                        />
-                    }
-                />
-
-                {/* ROW 2 */}
-                <Box
-                    gridColumn="span 7"
-                    gridRow="span 3"
-                    sx={{
-                        "& .MuiDataGrid-root": {
-                            border: "none",
-                        },
-                        "& .MuiDataGrid-cell": {
-                            borderBottom: "none",
-                        },
-                        "& .MuiDataGrid-columnHeaders": {
-                            backgroundColor: theme.palette.background.alt,
-                            color: theme.palette.secondary[100],
-                            borderBottom: "none",
-                        },
-                        "& .MuiDataGrid-virtualScroller": {
-                            backgroundColor: theme.palette.primary.light,
-                        },
-                        "& .MuiDataGrid-footerContainer": {
-                            backgroundColor: theme.palette.background.alt,
-                            color: theme.palette.secondary[100],
-                            borderTop: "none",
-                        },
-                        "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
-                            color: `${theme.palette.secondary[200]} !important`,
-                        },
-                    }}
-                >
-                    <DataGrid
-                        loading={!data}
-                        getRowId={(row) => row._id}
-                        rows={(data && data.upcomingEvents) || []}
-                        columns={columns}
+        <>
+            {data && (
+                <Box m="1.5rem 2.5rem">
+                    <Header
+                        title="DASHBOARD"
+                        subtitle="Welcome to your dashboard"
                     />
-                </Box>
-                <Box
-                    gridColumn="span 5"
-                    gridRow="span 3"
-                    backgroundColor={theme.palette.background.alt}
-                    p="1.5rem"
-                    borderRadius="0.55rem"
-                >
-                    <Typography
-                        variant="h6"
-                        sx={{ color: theme.palette.secondary[100] }}
+
+                    <Box
+                        mt="20px"
+                        display="grid"
+                        gridTemplateColumns="repeat(12, 1fr)"
+                        gridAutoRows="160px"
+                        gap="20px"
+                        sx={{
+                            "& > div": {
+                                gridColumn: isNonMediumScreens
+                                    ? undefined
+                                    : "span 12",
+                            },
+                        }}
                     >
-                        Events By Committees
-                    </Typography>
-                    <BreakdownChart />
-                    <Typography
-                        p="0 0.6rem"
-                        fontSize="0.8rem"
-                        sx={{ color: theme.palette.secondary[200] }}
-                    >
-                        Breakdown of information via Committees for Events
-                        organized for this year.
-                    </Typography>
+                        {/* ROW 1 */}
+                        <StatBox
+                            title="Total Events"
+                            value={data && data.approvedEventsCount}
+                            description="This Year"
+                            icon={
+                                <EventIcon
+                                    sx={{
+                                        color: theme.palette.secondary[300],
+                                        fontSize: "26px",
+                                    }}
+                                />
+                            }
+                        />
+                        <StatBox
+                            title="Pending"
+                            value={data && data.unapprovedEventsCount}
+                            description="Not Approved"
+                            icon={
+                                <PendingActionsIcon
+                                    sx={{
+                                        color: theme.palette.secondary[300],
+                                        fontSize: "26px",
+                                    }}
+                                />
+                            }
+                        />
+                        <Box
+                            gridColumn="span 8"
+                            gridRow="span 2"
+                            backgroundColor={theme.palette.background.alt}
+                            p="1rem"
+                            borderRadius="0.55rem"
+                        >
+                            <OverallStats data={data.eventsPerMonth} />
+                        </Box>
+                        <StatBox
+                            title="Management"
+                            value={data && data.adminsCount}
+                            description="Members"
+                            icon={
+                                <ManageAccountsIcon
+                                    sx={{
+                                        color: theme.palette.secondary[300],
+                                        fontSize: "26px",
+                                    }}
+                                />
+                            }
+                        />
+                        <StatBox
+                            title="Committees"
+                            value={data && data.committeesCount}
+                            description="Total"
+                            icon={
+                                <Diversity3Outlined
+                                    sx={{
+                                        color: theme.palette.secondary[300],
+                                        fontSize: "26px",
+                                    }}
+                                />
+                            }
+                        />
+
+                        {/* ROW 2 */}
+                        <Box
+                            gridColumn="span 7"
+                            gridRow="span 3"
+                            sx={{
+                                "& .MuiDataGrid-root": {
+                                    border: "none",
+                                },
+                                "& .MuiDataGrid-cell": {
+                                    borderBottom: "none",
+                                },
+                                "& .MuiDataGrid-columnHeaders": {
+                                    backgroundColor:
+                                        theme.palette.background.alt,
+                                    color: theme.palette.secondary[100],
+                                    borderBottom: "none",
+                                },
+                                "& .MuiDataGrid-virtualScroller": {
+                                    backgroundColor:
+                                        theme.palette.primary.light,
+                                },
+                                "& .MuiDataGrid-footerContainer": {
+                                    backgroundColor:
+                                        theme.palette.background.alt,
+                                    color: theme.palette.secondary[100],
+                                    borderTop: "none",
+                                },
+                                "& .MuiDataGrid-toolbarContainer .MuiButton-text":
+                                    {
+                                        color: `${theme.palette.secondary[200]} !important`,
+                                    },
+                            }}
+                        >
+                            <DataGrid
+                                loading={!data}
+                                getRowId={(row) => row._id}
+                                rows={(data && data.upcomingEvents) || []}
+                                columns={columns}
+                            />
+                        </Box>
+                        <Box
+                            gridColumn="span 5"
+                            gridRow="span 3"
+                            backgroundColor={theme.palette.background.alt}
+                            p="1.5rem"
+                            borderRadius="0.55rem"
+                        >
+                            <Typography
+                                variant="h6"
+                                sx={{ color: theme.palette.secondary[100] }}
+                            >
+                                Events By Committees
+                            </Typography>
+                            <BreakdownChart data={data.eventsPerCommittee} />
+                            <Typography
+                                p="0 0.6rem"
+                                fontSize="0.8rem"
+                                sx={{ color: theme.palette.secondary[200] }}
+                            >
+                                Breakdown of information via Committees for
+                                Events organized for this year.
+                            </Typography>
+                        </Box>
+                    </Box>
                 </Box>
-            </Box>
-        </Box>
+            )}
+        </>
     );
 };
 
