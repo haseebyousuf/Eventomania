d70c725b81be8dfe8f22210a5c91951a37147801
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-06-03 11:28:14 +0530

    fix: refactored controllers

--- server/routes/dashboardRoutes.js
+++ server/routes/dashboardRoutes.js
@@ -1,12 +1,11 @@
 import express from 'express';
-import {adminDashboardStats, dashboardStatsByCommittee, eventsPerCommittee} from "../controllers/dashboardController.js"
+import {adminDashboardStats, committeeDashboardStats} from "../controllers/dashboardController.js"
 
 const router = express.Router();
 
 
 router.get("/adminDashboardStats",adminDashboardStats );
-router.get("/dashboardStatsByCommittee",dashboardStatsByCommittee );
-router.get("/eventsPerCommittee",eventsPerCommittee );
+router.post("/committeeDashboardStats",committeeDashboardStats );
 
 
 export default router;
