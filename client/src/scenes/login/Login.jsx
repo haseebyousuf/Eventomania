c6b61fe1fc3229bfdded14deb7c23d59ff4699eb
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-06-03 11:23:25 +0530

    fixed width of container

--- client/src/scenes/login/Login.jsx
+++ client/src/scenes/login/Login.jsx
@@ -4,31 +4,31 @@ import LoginForm from "./LoginForm";
 import HomeNavbar from "../../components/HomeNavbar";
 
 const Login = () => {
-  const isNonMobile = useMediaQuery("(min-width: 600px)");
-  // const theme = useTheme();
+    const isNonMobile = useMediaQuery("(min-width: 600px)");
+    // const theme = useTheme();
 
-  return (
-    <Box
-      sx={{
-        height: "100vh",
-      }}
-    >
-      <HomeNavbar />
+    return (
+        <Box
+            sx={{
+                height: "100vh",
+            }}
+        >
+            <HomeNavbar />
 
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "90vh",
-        }}
-      >
-        <Box width={isNonMobile ? "35%" : "90%"}>
-          <LoginForm />
+            <Box
+                sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: "90vh",
+                }}
+            >
+                <Box width={isNonMobile ? "30%" : "90%"}>
+                    <LoginForm />
+                </Box>
+            </Box>
         </Box>
-      </Box>
-    </Box>
-  );
+    );
 };
 
 export default Login;
