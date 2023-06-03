8689d85b621025511ee13bd626857bf3151e1a87
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-06-03 11:19:25 +0530

    added: valueGetter property for uniqueId

--- client/src/scenes/AudienceDetails/AudienceDetails.jsx
+++ client/src/scenes/AudienceDetails/AudienceDetails.jsx
@@ -56,6 +56,7 @@ const AudienceDetails = () => {
             headerName: "Reg. No. / Employee ID",
             minWidth: 160,
             flex: 1,
+            valueGetter: (params) => params.row.regNo || params.row.employeeId,
             renderCell: (params) => {
                 return params.row.regNo || params.row.employeeId;
             },
