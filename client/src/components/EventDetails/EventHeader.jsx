4cc95c940a36948dcfe4c0a76f72fba774c976ee
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-07-11 18:05:27 +0530

    refactored

--- client/src/components/EventDetails/EventHeader.jsx
+++ client/src/components/EventDetails/EventHeader.jsx
@@ -13,118 +13,130 @@ import moment from "moment";
 import React from "react";
 
 const EventHeader = ({
-    name,
-    banner,
-    startDate,
-    endDate,
-    venue,
-    organizedBy,
+  name,
+  banner,
+  startDate,
+  endDate,
+  venue,
+  organizedBy,
+  createdBy,
+  dialog,
 }) => {
-    const isNonMobile = useMediaQuery("(min-width: 600px)");
-    const Item = styled(Paper)(({ theme }) => ({
-        backgroundImage: "none",
-        backgroundColor: theme.palette.background.alt,
-        padding: theme.spacing(1),
-        textAlign: "center",
-        color: theme.palette.text.secondary,
-    }));
-    return (
-        <>
-            <img
-                src={`${process.env.REACT_APP_BASE_URL}/assets/${banner}`}
-                alt="banner"
-                width={isNonMobile ? "90%" : "100%"}
-            />
-            <Typography
-                mt={2}
-                fontSize={isNonMobile ? "2rem" : "1.5rem"}
-                varient="h1"
-                fontWeight="bold"
-                width={isNonMobile ? "90%" : "100%"}
-            >
-                {name}
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundImage: "none",
+    backgroundColor: theme.palette.background.alt,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  }));
+  return (
+    <>
+      <img
+        src={`${process.env.REACT_APP_BASE_URL}/assets/${banner}`}
+        alt='banner'
+        width={isNonMobile ? "90%" : "100%"}
+      />
+      <Typography
+        mt={2}
+        fontSize={isNonMobile ? "2rem" : "1.5rem"}
+        variant='h1'
+        fontWeight='bold'
+        width={isNonMobile ? "90%" : "100%"}
+        color='secondary'
+      >
+        {name}
+      </Typography>
+      <Stack
+        width={isNonMobile ? "90%" : "100%"}
+        mt={2}
+        mb={2}
+        direction={isNonMobile ? "row" : "column"}
+        divider={<Divider orientation='vertical' flexItem />}
+        spacing={1}
+      >
+        <Item
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            gap: "0.5rem",
+          }}
+        >
+          <TodayIcon color='secondary' />{" "}
+          <Typography fontSize={!isNonMobile && "1rem"}>
+            {isNonMobile
+              ? `${moment(startDate).format("MMMM Do YYYY, h:mm A")} - ${moment(
+                  endDate
+                ).format("MMMM Do YYYY, h:mm A")}`
+              : moment(startDate).format("MMMM Do YYYY, h:mm A")}
+          </Typography>
+        </Item>
+        {!isNonMobile && (
+          <Item
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              gap: "0.5rem",
+            }}
+          >
+            <EventIcon color='secondary' />{" "}
+            <Typography fontSize={!isNonMobile && "1rem"}>
+              {moment(endDate).format("MMMM Do YYYY, h:mm A")}
             </Typography>
-            <Stack
-                width={isNonMobile ? "90%" : "100%"}
-                mt={2}
-                mb={2}
-                direction={isNonMobile ? "row" : "column"}
-                divider={<Divider orientation="vertical" flexItem />}
-                spacing={1}
-            >
-                <Item
-                    sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        gap: "0.5rem",
-                    }}
-                >
-                    <TodayIcon color="secondary" />{" "}
-                    <Typography fontSize={!isNonMobile && "1rem"}>
-                        {isNonMobile
-                            ? `${moment(startDate).format(
-                                  "MMMM Do YYYY, h:mm A"
-                              )} - ${moment(endDate).format(
-                                  "MMMM Do YYYY, h:mm A"
-                              )}`
-                            : moment(startDate).format("MMMM Do YYYY, h:mm A")}
-                    </Typography>
-                </Item>
-                {!isNonMobile && (
-                    <Item
-                        sx={{
-                            display: "flex",
-                            alignItems: "center",
-                            gap: "0.5rem",
-                        }}
-                    >
-                        <EventIcon color="secondary" />{" "}
-                        <Typography fontSize={!isNonMobile && "1rem"}>
-                            {moment(endDate).format("MMMM Do YYYY, h:mm A")}
-                        </Typography>
-                    </Item>
-                )}
-                <Item
-                    sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        gap: "0.5rem",
-                    }}
-                >
-                    <LocationOnIcon color="secondary" />
-                    <Typography fontSize={!isNonMobile && "1rem"}>
-                        {venue}
-                    </Typography>
-                </Item>
-            </Stack>
-            <Stack
-                width={isNonMobile ? "90%" : "100%"}
-                mt={2}
-                mb={2}
-                direction={isNonMobile ? "row" : "column"}
-                divider={<Divider orientation="vertical" flexItem />}
-                spacing={1}
-            >
-                <Item
-                    sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        gap: "0.5rem",
-                    }}
-                >
-                    <Typography fontSize={!isNonMobile && "1rem"}>
-                        Organized By:
-                    </Typography>
-                    <Typography
-                        color="secondary"
-                        fontSize={!isNonMobile && "1rem"}
-                    >
-                        {organizedBy}
-                    </Typography>
-                </Item>
-            </Stack>
-        </>
-    );
+          </Item>
+        )}
+        <Item
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            gap: "0.5rem",
+          }}
+        >
+          <LocationOnIcon color='secondary' />
+          <Typography fontSize={!isNonMobile && "1rem"}>{venue}</Typography>
+        </Item>
+      </Stack>
+      <Stack
+        width={isNonMobile ? "90%" : "100%"}
+        mt={2}
+        mb={2}
+        direction={isNonMobile ? "row" : "column"}
+        divider={<Divider orientation='vertical' flexItem />}
+        spacing={1}
+      >
+        <Item
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            gap: "0.5rem",
+          }}
+        >
+          <Typography fontSize={!isNonMobile && "1rem"}>
+            Organized By:
+          </Typography>
+          <Typography color='secondary' fontSize={!isNonMobile && "1rem"}>
+            {organizedBy}
+          </Typography>
+        </Item>
+        {dialog && (
+          <Item
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              gap: "0.5rem",
+            }}
+          >
+            <Typography fontSize={!isNonMobile && "1rem"}>
+              Created By:
+            </Typography>
+            <Typography color='secondary' fontSize={!isNonMobile && "1rem"}>
+              {createdBy}
+            </Typography>
+          </Item>
+        )}
+      </Stack>
+    </>
+  );
 };
 
 export default EventHeader;
