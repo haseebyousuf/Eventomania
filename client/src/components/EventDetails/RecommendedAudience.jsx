1067f7ab9773e18397a26179845e5b6c99a7c97c
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-07-11 18:05:41 +0530

    refactored

--- client/src/components/EventDetails/RecommendedAudience.jsx
+++ client/src/components/EventDetails/RecommendedAudience.jsx
@@ -12,69 +12,56 @@ const RecommendedAudience = ({ event }) => {
     const theme = useTheme();
     const isNonMobile = useMediaQuery("(min-width: 600px)");
     return (
-        <>
-            {/* <Box
-        mt={2}
-        mb={2}
-        height="3rem"
-        pl={1}
-        width={isNonMobile ? "90%" : "100%"}
-        sx={{
-          borderRadius: "5px",
-          backgroundColor: theme.palette.background.alt,
-        }}
-      > */}
-            <Typography
-                mt={2}
-                fontSize="1.7rem"
-                variant="h2"
-                fontWeight="bold"
-                color="secondary"
-            >
-                This Event Is For
-            </Typography>
-            {/* </Box> */}
-            <Box mt={1} pb={2} mb={2} width={isNonMobile ? "90%" : "100%"}>
-                <Box
-                    sx={{
-                        display: "flex",
-                        gap: 2,
-                        "& .MuiCardContent-root:last-child": {
-                            paddingBottom: "16px",
-                        },
-                    }}
+      <>
+        <Typography
+          mt={2}
+          fontSize='1.7rem'
+          variant='h2'
+          fontWeight='bold'
+          color='secondary'
+        >
+          Recommended Audience
+        </Typography>
+        <Box mt={1} pb={2} mb={2} width={isNonMobile ? "90%" : "100%"}>
+          <Box
+            sx={{
+              display: "flex",
+              gap: 2,
+              "& .MuiCardContent-root:last-child": {
+                paddingBottom: "16px",
+              },
+            }}
+          >
+            {event &&
+              event.recommendedAudience.split(",").map((e) => (
+                <Card
+                  key={e}
+                  sx={{
+                    borderRadius: "0.55rem",
+                    backgroundImage: "none",
+                    backgroundColor: theme.palette.background.alt,
+                  }}
                 >
-                    {event &&
-                        event.recommendedAudience.split(",").map((e) => (
-                            <Card
-                                key={e}
-                                sx={{
-                                    borderRadius: "0.55rem",
-                                    backgroundImage: "none",
-                                    backgroundColor:
-                                        theme.palette.background.alt,
-                                }}
-                            >
-                                <CardContent
-                                    d="flex"
-                                    sx={{
-                                        justifyContent: "center",
-                                        alignItems: "center",
-                                    }}
-                                >
-                                    <Typography
-                                        fontSize="1.1rem"
-                                        textTransform="uppercase"
-                                        variant="h5"
-                                    >
-                                        {e}
-                                    </Typography>
-                                </CardContent>
-                            </Card>
-                        ))}
-                </Box>
-            </Box>
-        </>
+                  <CardContent
+                    d='flex'
+                    sx={{
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Typography
+                      fontSize='1.1rem'
+                      textTransform='uppercase'
+                      variant='h5'
+                    >
+                      {e}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              ))}
+          </Box>
+        </Box>
+      </>
     );
 };
 
