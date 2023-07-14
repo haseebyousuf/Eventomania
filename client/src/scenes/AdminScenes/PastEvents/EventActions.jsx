aa5da6e56e92c80e677f14eceff91fc5044196a3
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-07-14 14:24:09 +0530

    added confirm before delete

--- client/src/scenes/AdminScenes/PastEvents/EventActions.jsx
+++ client/src/scenes/AdminScenes/PastEvents/EventActions.jsx
@@ -8,104 +8,105 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const EventActions = ({
-    getEvents,
-    snackbarData,
-    users,
-    setSnackbarData,
-    params,
+  getEvents,
+  snackbarData,
+  users,
+  setSnackbarData,
+  params,
 }) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [openDialog, setOpenDialog] = React.useState(false);
 
-    const [openDialog, setOpenDialog] = React.useState(false);
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+  };
+  const handleOpenDialog = () => {
+    setOpenDialog(true);
+  };
+  const handleDelete = async (id) => {
+    const choice = window.confirm("Want to delete Past Event?");
+    if (choice) {
+      try {
+        const response = await axios({
+          method: "post",
 
-    const handleCloseDialog = () => {
-        setOpenDialog(false);
-    };
-    const handleOpenDialog = () => {
-        setOpenDialog(true);
-    };
-    const handleDelete = async (id) => {
-        try {
-            const response = await axios({
-                method: "post",
-
-                url: `${process.env.REACT_APP_BASE_URL}/events/deleteEvent`,
-                headers: { "Content-Type": "application/json" },
-                data: JSON.stringify({ eventId: id }),
-            });
-            const responseData = await response.data;
-            if (responseData) {
-                getEvents();
-                setSnackbarData({
-                    ...snackbarData,
-                    open: true,
-                    message: "Event Deleted Successfully",
-                    severity: "error",
-                });
-                setTimeout(() => {
-                    setSnackbarData({
-                        ...snackbarData,
-                        open: false,
-                    });
-                }, 4000);
-            }
-        } catch (error) {
+          url: `${process.env.REACT_APP_BASE_URL}/events/deleteEvent`,
+          headers: { "Content-Type": "application/json" },
+          data: JSON.stringify({ eventId: id }),
+        });
+        const responseData = await response.data;
+        if (responseData) {
+          getEvents();
+          setSnackbarData({
+            ...snackbarData,
+            open: true,
+            message: "Event Deleted Successfully",
+            severity: "error",
+          });
+          setTimeout(() => {
             setSnackbarData({
-                ...snackbarData,
-                open: true,
-                message: "There was an error deleting the event",
-                severity: "error",
+              ...snackbarData,
+              open: false,
             });
-            setTimeout(() => {
-                setSnackbarData({
-                    ...snackbarData,
-                    open: false,
-                });
-            }, 4000);
+          }, 4000);
         }
-    };
-    return (
-        <Box>
-            <EventDialog
-                handleOpenDialog={handleOpenDialog}
-                handleCloseDialog={handleCloseDialog}
-                openDialog={openDialog}
-                setOpenDialog={setOpenDialog}
-                params={params}
-            />
-            <Tooltip title="View Event Details">
-                <IconButton onClick={handleOpenDialog}>
-                    <VisibilityIcon color="info" />
-                </IconButton>
-            </Tooltip>
-            <Tooltip title="View Audience Details">
-                <IconButton
-                    onClick={() => {
-                        navigate(`/Registrations/${params.row._id}`, {
-                            state: {
-                                audience: users.filter(
-                                    (user) =>
-                                        user.event[0].id === params.row._id
-                                ),
-                                eventDetails: params.row.name,
-                            },
-                        });
-                    }}
-                >
-                    <GroupsIcon color="success" />
-                </IconButton>
-            </Tooltip>
-            <Tooltip title="Delete This Event">
-                <IconButton
-                    onClick={() => {
-                        handleDelete(params.id);
-                    }}
-                >
-                    <Delete color="error" />
-                </IconButton>
-            </Tooltip>
-        </Box>
-    );
+      } catch (error) {
+        setSnackbarData({
+          ...snackbarData,
+          open: true,
+          message: "There was an error deleting the event",
+          severity: "error",
+        });
+        setTimeout(() => {
+          setSnackbarData({
+            ...snackbarData,
+            open: false,
+          });
+        }, 4000);
+      }
+    }
+  };
+  return (
+    <Box>
+      <EventDialog
+        handleOpenDialog={handleOpenDialog}
+        handleCloseDialog={handleCloseDialog}
+        openDialog={openDialog}
+        setOpenDialog={setOpenDialog}
+        params={params}
+      />
+      <Tooltip title='View Event Details'>
+        <IconButton onClick={handleOpenDialog}>
+          <VisibilityIcon color='info' />
+        </IconButton>
+      </Tooltip>
+      <Tooltip title='View Audience Details'>
+        <IconButton
+          onClick={() => {
+            navigate(`/Registrations/${params.row._id}`, {
+              state: {
+                audience: users.filter(
+                  (user) => user.event[0].id === params.row._id
+                ),
+                eventDetails: params.row.name,
+              },
+            });
+          }}
+        >
+          <GroupsIcon color='success' />
+        </IconButton>
+      </Tooltip>
+      <Tooltip title='Delete This Event'>
+        <IconButton
+          onClick={() => {
+            handleDelete(params.id);
+          }}
+        >
+          <Delete color='error' />
+        </IconButton>
+      </Tooltip>
+    </Box>
+  );
 };
 
 export default EventActions;
