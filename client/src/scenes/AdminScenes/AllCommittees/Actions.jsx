5ec4b9a67f27ff2d496ede998a7dddd488b26f96
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-07-14 15:05:37 +0530

    added delete feature

--- client/src/scenes/AdminScenes/AllCommittees/Actions.jsx
+++ client/src/scenes/AdminScenes/AllCommittees/Actions.jsx
@@ -1,28 +1,65 @@
 import { Box, IconButton, Tooltip } from "@mui/material";
-import { Delete, Edit } from "@mui/icons-material";
-import RemoveRedEyeOutlinedIcon from "@mui/icons-material/RemoveRedEyeOutlined";
-import React from "react";
+import { Delete } from "@mui/icons-material";
+import axios from "axios";
 
-const Actions = ({ params }) => {
-    return (
-        <Box>
-            <Tooltip title="View Committee Details">
-                <IconButton onClick={() => {}}>
-                    <RemoveRedEyeOutlinedIcon color="info" />
-                </IconButton>
-            </Tooltip>
-            <Tooltip title="Edit This Committee">
-                <IconButton onClick={() => {}}>
-                    <Edit color="success" />
-                </IconButton>
-            </Tooltip>
-            <Tooltip title="Delete This Committee">
-                <IconButton onClick={() => {}}>
-                    <Delete color="error" />
-                </IconButton>
-            </Tooltip>
-        </Box>
+const Actions = ({ getCommittees, snackbarData, setSnackbarData, params }) => {
+  const handleDelete = async (id) => {
+    const choice = window.confirm(
+      "Are you sure you want to delete this committee?"
     );
+    if (choice) {
+      try {
+        const response = await axios({
+          method: "post",
+          url: `${process.env.REACT_APP_BASE_URL}/committee/deleteCommittee`,
+          headers: { "Content-Type": "application/json" },
+          data: JSON.stringify({ committeeId: id }),
+        });
+        const responseData = await response.data;
+        if (responseData) {
+          getCommittees();
+          setSnackbarData({
+            ...snackbarData,
+            open: true,
+            message: "Committee Deleted Successfully",
+            severity: "success",
+          });
+          setTimeout(() => {
+            setSnackbarData({
+              ...snackbarData,
+              open: false,
+            });
+          }, 4000);
+        }
+      } catch (error) {
+        setSnackbarData({
+          ...snackbarData,
+          open: true,
+          message: "There was an error deleting the Committee",
+          severity: "error",
+        });
+        setTimeout(() => {
+          setSnackbarData({
+            ...snackbarData,
+            open: false,
+          });
+        }, 4000);
+      }
+    }
+  };
+  return (
+    <Box>
+      <Tooltip title='Delete This Committee'>
+        <IconButton
+          onClick={() => {
+            handleDelete(params.id);
+          }}
+        >
+          <Delete color='error' />
+        </IconButton>
+      </Tooltip>
+    </Box>
+  );
 };
 
 export default Actions;
