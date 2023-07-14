1facc17cfb9cb853732be98dceffaa3d242b8cfc
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-07-14 15:07:17 +0530

    added deleteCommittee route

--- server/routes/committee.js
+++ server/routes/committee.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import { verifyToken } from '../middleware/auth.js';
-import {addCommittee,getCommittes} from "../controllers/committee.js"
+import {
+  addCommittee,
+  getCommittees,
+  deleteCommittee,
+} from "../controllers/committee.js";
 
 const router = express.Router();
 
 router.post("/add-committee", addCommittee);
-router.get("/get-committees", getCommittes);
+router.get("/get-committees", getCommittees);
+router.post("/deleteCommittee", deleteCommittee);
+
 export default router;
