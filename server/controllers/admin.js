febde200381b062ccdb310b1dc418a67fae210b5
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-07-15 20:06:51 +0530

    feat: refactored previous controllers and added controllers to delete convenors and members

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -71,48 +71,139 @@ export const createAdmin  = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 }
-export const addConvenor = async(req,res) => {
-  try{
-    const { name,email, password, committeeId, committeeName,role, mobile } = req.body;
-    const salt = await bcrypt.genSalt();
-    const passwordHash = await bcrypt.hash(password, salt);
-    const existingConvenor = await Admin.findOne({committeeId: committeeId});
-    if(existingConvenor){
-      const updatedConvenor = await Admin.findOneAndUpdate({ committeeId: committeeId },
-        { email, password: passwordHash, name, role, committeeName, committeeId, mobile },
+export const addConvenor = async (req, res) => {
+  try {
+    const { name, email, password, committeeId, committeeName, role, mobile } =
+      req.body;
+    //check if user with same email exits
+    const user = await Admin.findOne({ email: email });
+    if (user) {
+      res.status(400).json({ msg: "Email already exits" });
+    } else {
+      const salt = await bcrypt.genSalt();
+      const passwordHash = await bcrypt.hash(password, salt);
+      const existingConvenor = await Admin.findOne({
+        committeeId: committeeId,
+      });
+      if (existingConvenor) {
+        const updatedConvenor = await Admin.findOneAndUpdate(
+          { committeeId: committeeId },
+          {
+            email,
+            password: passwordHash,
+            name,
+            role,
+            committeeName,
+            committeeId,
+            mobile,
+          },
+          { new: true }
+        );
+        const filter = { _id: committeeId };
+        const update = {
+          convenorName: updatedConvenor.name,
+          convenorId: updatedConvenor._id,
+        };
+        const updatedCommittee = await Committee.findOneAndUpdate(
+          filter,
+          update,
+          { new: true }
+        );
+        res.status(201).json({ updatedConvenor, updatedCommittee });
+      } else {
+        const newConvenor = new Admin({
+          email,
+          password: passwordHash,
+          name,
+          role,
+          committeeName,
+          committeeId,
+          mobile,
+        });
+        const savedConvenor = await newConvenor.save();
+        const filter = { _id: committeeId };
+        const update = {
+          convenorName: savedConvenor.name,
+          convenorId: savedConvenor._id,
+        };
+        const updatedCommittee = await Committee.findOneAndUpdate(
+          filter,
+          update,
+          { new: true }
+        );
+        res.status(201).json({ savedConvenor, updatedCommittee });
+      }
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+export const deleteConvenor = async (req, res) => {
+  console.log(req.body);
+  try {
+    const { convenorId, committeeId } = req.body;
+    const deletedConvenor = await Admin.deleteOne({ _id: convenorId });
+    if (deletedConvenor) {
+      const filter = { _id: committeeId };
+      const update = { convenorName: "-", convenorId: "-" };
+      const updatedCommittee = await Committee.findOneAndUpdate(
+        filter,
+        update,
         { new: true }
       );
-      const filter= {_id: committeeId};
-    const update = {convenorName: updatedConvenor.name, convenorId: updatedConvenor._id};
-    const updatedCommittee = await Committee.findOneAndUpdate(filter,update, {new:true});
-    res.status(201).json( {updatedConvenor,updatedCommittee});
-
-    }else{
-      const newConvenor = new Admin({email, password:passwordHash, name, role, committeeName, committeeId, mobile});
-      const savedConvenor = await newConvenor.save();
-      const filter= {_id: committeeId};
-      const update = {convenorName: savedConvenor.name, convenorId: savedConvenor._id};
-      const updatedCommittee = await Committee.findOneAndUpdate(filter,update, {new:true});
-      res.status(201).json( {savedConvenor,updatedCommittee}); 
+      res.status(201).json({ msg: "Deleted Successfully" });
+    } else {
+      res.status(500).json({ error: error.message });
     }
-    
-    
+  } catch (error) {
+    res.status(500).json({ error: error.message, msg: error.message });
+  }
+};
 
-  }catch(err){
+export const addMember = async (req, res) => {
+  try {
+    const {
+      memberName,
+      memberEmail,
+      memberPassword,
+      committeeId,
+      committeeName,
+      role,
+      mobile,
+    } = req.body;
+    const user = await Admin.findOne({ email: memberEmail });
+    if (user) {
+      res.status(400).json({ msg: "Email already exits" });
+    } else {
+      const salt = await bcrypt.genSalt();
+      const passwordHash = await bcrypt.hash(memberPassword, salt);
+      const newMember = new Admin({
+        email: memberEmail,
+        password: passwordHash,
+        name: memberName,
+        role,
+        committeeName,
+        committeeId,
+        mobile,
+      });
+      const savedMember = await newMember.save();
+      res.status(201).json({ savedMember });
+    }
+  } catch (err) {
     res.status(500).json({ error: err.message });
   }
-}
+};
 
-export const addMember = async(req,res) => {
-  try{
-    const { name,email, password, committeeId, committeeName,role, mobile } = req.body;
-    const salt = await bcrypt.genSalt();
-    const passwordHash = await bcrypt.hash(password, salt);
-    const newMember = new Admin({email, password:passwordHash, name, role, committeeName, committeeId, mobile});
-    const savedMember = await newMember.save();
-    res.status(201).json( {savedMember});     
-
-  }catch(err){
-    res.status(500).json({ error: err.message });
+export const deleteMember = async (req, res) => {
+  try {
+    const { memberId } = req.body;
+    const deletedMember = await Admin.deleteOne({ _id: memberId });
+    if (deletedMember) {
+      res.status(201).json({ msg: "Deleted Successfully" });
+    } else {
+      res.status(500).json({ error: error.message });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
-}
+};
