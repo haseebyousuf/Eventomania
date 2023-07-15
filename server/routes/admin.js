0c5d9b5f8b1f8ce7c903a9a98c583889b0418b4c
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-07-15 20:07:30 +0530

    feat: added delete convenor and member routes

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,16 +1,29 @@
 import express from 'express';
 import {verifyToken} from "../middleware/auth.js";
-import {createAdmin, getConvenors, verifyAdmin, addConvenor,getMembers,addMember, getCommitteeMembers} from "../controllers/admin.js"
+import {
+  createAdmin,
+  getConvenors,
+  verifyAdmin,
+  addConvenor,
+  getMembers,
+  addMember,
+  getCommitteeMembers,
+  deleteConvenor,
+  deleteMember,
+} from "../controllers/admin.js";
 
 const router = express.Router();
 
-router.post("/verify", verifyAdmin)
+router.post("/verify", verifyAdmin);
 router.post("/createAdmin", createAdmin);
-router.get("/get-convenors", getConvenors); 
+router.get("/get-convenors", getConvenors);
 router.post("/addConvenor", addConvenor);
+router.post("/deleteConvenor", deleteConvenor);
 router.get("/getMembers", getMembers);
 router.post("/committeeMembers", getCommitteeMembers);
 router.post("/addMember", addMember);
+router.post("/deleteMember", deleteMember);
+
 // router.get("/verifyToken", verifyToken)
 
 export default router;
