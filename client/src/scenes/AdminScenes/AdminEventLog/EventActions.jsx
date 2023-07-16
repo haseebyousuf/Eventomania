e4f3e3ca191294887e981099aa1960214d591dce
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-07-16 17:33:36 +0530

    refactor

--- client/src/scenes/AdminScenes/AdminEventLog/EventActions.jsx
+++ client/src/scenes/AdminScenes/AdminEventLog/EventActions.jsx
@@ -7,39 +7,38 @@ import { useNavigate } from "react-router-dom";
 const EventActions = ({ users, data, params }) => {
     const navigate = useNavigate();
 
-    return (
-        <Box>
-            <Tooltip title="View Audience Details">
-                <IconButton
-                    onClick={() => {
-                        navigate(`/Registrations/${params.row._id}`, {
-                            state: {
-                                audience: users.filter(
-                                    (user) =>
-                                        user.event[0].id === params.row._id
-                                ),
-                                eventDetails: params.row.name,
-                            },
-                        });
-                    }}
-                >
-                    <GroupsIcon color="success" />
-                </IconButton>
-            </Tooltip>
-            <Tooltip title="Download Report">
-                <IconButton onClick={() => {}}>
-                    <a
-                        href={`${process.env.REACT_APP_BASE_URL}/assets/${params.row.reportName}`}
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        {" "}
-                        <DownloadForOfflineIcon color="info" />
-                    </a>
-                </IconButton>
-            </Tooltip>
-        </Box>
-    );
+  return (
+    <Box>
+      <Tooltip title='View Audience Details'>
+        <IconButton
+          onClick={() => {
+            navigate(`/Registrations/${params.row._id}`, {
+              state: {
+                audience: users.filter(
+                  (user) => user.event[0].id === params.row._id
+                ),
+                eventDetails: params.row.name,
+              },
+            });
+          }}
+        >
+          <GroupsIcon color='success' />
+        </IconButton>
+      </Tooltip>
+      <Tooltip title='Download Report'>
+        <IconButton onClick={() => {}}>
+          <a
+            href={`${process.env.REACT_APP_BASE_URL}/assets/${params.row.reportName}`}
+            target='_blank'
+            rel='noreferrer'
+          >
+            {" "}
+            <DownloadForOfflineIcon color='info' />
+          </a>
+        </IconButton>
+      </Tooltip>
+    </Box>
+  );
 };
 
 export default EventActions;
