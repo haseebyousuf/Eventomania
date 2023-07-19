be5ef6cec811bfe887e5e7b82ad1fc99f0e60556
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-07-19 17:52:06 +0530

    fix: changed error message

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,77 +2,92 @@ import User from "../models/User.js";
 
 export const registerStudent = async (req, res) => {
   try {
-      const { name, mobileNo, email, regNo, semester, course, department, event, type } =
-          req.body;
-          //check if student already registered for the given event
-      const user = await User.find({
-        $or: [
-          { regNo: regNo },
-          { email: email }
-        ],
-          event: { $elemMatch: { id: event.id } },
-      });
-      //return error if student already registered
-      if (user.length > 0) {
-          return res.status(400).json({ msg: "You Have Already Registered!" });
-      }
-      //create new user
-      const newUser = new User({
-          name,
-          regNo,
-          phoneNo: mobileNo,
-          email,
-          semester,
-          course,
-          department,
-          event,
-          type,
-      });
-      const savedUser = await newUser.save();
-      res.status(201).json(savedUser);
+    const {
+      name,
+      mobileNo,
+      email,
+      regNo,
+      semester,
+      course,
+      department,
+      event,
+      type,
+    } = req.body;
+    //check if student already registered for the given event
+    const user = await User.find({
+      $or: [{ regNo: regNo }, { email: email }],
+      event: { $elemMatch: { id: event.id } },
+    });
+    //return error if student already registered
+    if (user.length > 0) {
+      return res
+        .status(400)
+        .json({ msg: "You Have Already Registered for this event!" });
+    }
+    //create new user
+    const newUser = new User({
+      name,
+      regNo,
+      phoneNo: mobileNo,
+      email,
+      semester,
+      course,
+      department,
+      event,
+      type,
+    });
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
   } catch (error) {
-      res.status(500).json({ error: error.message });
+    res.status(500).json({ error: error.message });
   }
 };
 export const registerFaculty = async (req, res) => {
   try {
-      const { name,mobileNo, email, employeeId, designation, department, event, type } =
-          req.body;
-          //check if faculty already registered for the given event
-      const user = await User.find({
-        $or: [
-          { employeeId: employeeId },
-          { email: email }
-        ],
-          event: { $elemMatch: { id: event.id } },
-      });
-      //return error if faculty already registered
-      if (user.length > 0) {
-          return res.status(400).json({ msg: "You Have Already Registered!" });
-      }
-      //create new user
-      const newUser = new User({
-          name,
-          employeeId,
-          phoneNo: mobileNo,
-          email,
-          designation,
-          department,
-          event,
-          type,
-      });
-      const savedUser = await newUser.save();
-      res.status(201).json(savedUser);
+    const {
+      name,
+      mobileNo,
+      email,
+      employeeId,
+      designation,
+      department,
+      event,
+      type,
+    } = req.body;
+    //check if faculty already registered for the given event
+    const user = await User.find({
+      $or: [{ employeeId: employeeId }, { email: email }],
+      event: { $elemMatch: { id: event.id } },
+    });
+    //return error if faculty already registered
+    if (user.length > 0) {
+      return res
+        .status(400)
+        .json({ msg: "You Have Already Registered for this event!" });
+    }
+    //create new user
+    const newUser = new User({
+      name,
+      employeeId,
+      phoneNo: mobileNo,
+      email,
+      designation,
+      department,
+      event,
+      type,
+    });
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
   } catch (error) {
-      res.status(500).json({ error: error.message });
+    res.status(500).json({ error: error.message });
   }
 };
 
 export const getUsers = async (req, res) => {
   try {
-      const users = await User.find();
-      res.status(200).json(users);
+    const users = await User.find();
+    res.status(200).json(users);
   } catch (error) {
-      res.status(500).json({ error: error.message });
+    res.status(500).json({ error: error.message });
   }
-}
+};
