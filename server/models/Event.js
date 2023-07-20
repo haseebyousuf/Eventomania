eef77b9e4e9bf8e1f5cd67144d4cb38d20f4c877
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-07-20 17:48:00 +0530

    add photos and isPhotoUploaded field

--- server/models/Event.js
+++ server/models/Event.js
@@ -1,64 +1,77 @@
 import mongoose from "mongoose";
 
-const EventSchema = new mongoose.Schema({
-  name:{
-    type:String,
-    required: true,
-  },
-  venue:{
-    type:String,
-    required: true,
-  },
-  startDate:{
-    type: String,
-    required: true,
-    
-  },
-  endDate:{
-    type: String,
-    required: true,
-  },
-  description:{
-    type: String,
-    required: true,
-  },
-  recommendedAudience:{
-    type: String,
-    required: true,
-  },
-  bannerPath: String,
-  bannerName: String,
-  orderPath: String,
-  orderName: String,
-  reportPath: String,
-  reportName: String,
-  committee:[{
-      id:{
-        type:String,
-      },
-      name: {
-        type: String,
-      },
-  }],
-  createdBy:[{
-    id:{
-      type:String,
-    },
+const EventSchema = new mongoose.Schema(
+  {
     name: {
       type: String,
+      required: true,
     },
-}],
-  isPublished:{
-    type: Boolean,
-    default: "false",
-  },isApproved:{
-    type: Boolean,
-    default: "false",
-  },status:{
-    type: Boolean,
-    default: "false",
-  }
-}, { timestamps: true });
+    venue: {
+      type: String,
+      required: true,
+    },
+    startDate: {
+      type: String,
+      required: true,
+    },
+    endDate: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    recommendedAudience: {
+      type: String,
+      required: true,
+    },
+    photos: Array,
+    isPhotoUploaded: {
+      type: Boolean,
+      default: "false",
+    },
+    bannerPath: String,
+    bannerName: String,
+    orderPath: String,
+    orderName: String,
+    reportPath: String,
+    reportName: String,
+    committee: [
+      {
+        id: {
+          type: String,
+        },
+        name: {
+          type: String,
+        },
+      },
+    ],
+    createdBy: [
+      {
+        id: {
+          type: String,
+        },
+        name: {
+          type: String,
+        },
+      },
+    ],
+    isPublished: {
+      type: Boolean,
+      default: "false",
+    },
+    isApproved: {
+      type: Boolean,
+      default: "false",
+    },
+    status: {
+      type: Boolean,
+      default: "false",
+    },
+  },
+  { timestamps: true }
+);
 
 const Event = mongoose.model("Event", EventSchema);
 
