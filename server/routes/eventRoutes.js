0472e81bf8c5794ef39c31d239df0d9afd6a8b51
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-07-22 15:11:27 +0530

    feat: create sendCertificate route

--- server/routes/eventRoutes.js
+++ server/routes/eventRoutes.js
@@ -1,15 +1,24 @@
-import express from 'express';
-import {getUnApprovedEvents, deleteEvent, getEvents,togglePublish,approveEvent, getPublishedEvents, getApprovedEvents} from "../controllers/eventController.js"
-
+import express from "express";
+import {
+  getUnApprovedEvents,
+  deleteEvent,
+  getEvents,
+  togglePublish,
+  approveEvent,
+  getPublishedEvents,
+  getApprovedEvents,
+  sendCertificate,
+} from "../controllers/eventController.js";
 const router = express.Router();
 
-router.get("/getUnApprovedEvents", getUnApprovedEvents)
-router.get("/getApprovedEvents", getApprovedEvents)
-router.post("/approveEvent", approveEvent)
-router.get("/getPublishedEvents", getPublishedEvents)
+router.get("/getUnApprovedEvents", getUnApprovedEvents);
+router.get("/getApprovedEvents", getApprovedEvents);
+router.post("/approveEvent", approveEvent);
+router.get("/getPublishedEvents", getPublishedEvents);
 router.get("/getEvents", getEvents);
-router.post("/togglePublish", togglePublish)
-router.post("/deleteEvent", deleteEvent)
+router.post("/togglePublish", togglePublish);
+router.post("/deleteEvent", deleteEvent);
+router.post("/sendCertificate", sendCertificate);
 // router.get("/eventsPerMonth", eventsPerMonth)
 
 export default router;
