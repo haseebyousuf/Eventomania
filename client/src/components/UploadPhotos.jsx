932c2d9569275044f4cbc33168f9db79a0b4bea9
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-07-22 15:06:50 +0530

    feat: added tostify promise and fix button style

--- client/src/components/UploadPhotos.jsx
+++ client/src/components/UploadPhotos.jsx
@@ -3,11 +3,13 @@ import * as yup from "yup";
 import React from "react";
 import { CloudUploadOutlined } from "@mui/icons-material";
 import Dropzone from "react-dropzone";
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import axios from "axios";
 import { toast } from "react-toastify";
 
 const UploadPhotos = ({ id, getEvents }) => {
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+
   const photosSchema = yup.object().shape({
     photos: yup
       .array()
@@ -16,48 +18,36 @@ const UploadPhotos = ({ id, getEvents }) => {
   });
 
   const handleFormSubmit = async (values, onSubmitProps) => {
+    setButtonDisabled(true);
     try {
-      console.log(values);
       const formData = new FormData();
       for (let photo of values.photos) {
         formData.append("photos", photo);
       }
       formData.append("id", id);
-      const photosResponse = await axios({
-        method: "post",
-        url: `${process.env.REACT_APP_BASE_URL}/event/uploadPhotos`,
-        headers: { "Content-Type": "application/JSON" },
-        data: formData,
-      });
-      const updatedEvent = await photosResponse.data;
-      onSubmitProps.resetForm();
+      const promise = toast.promise(
+        axios({
+          method: "post",
+          url: `${process.env.REACT_APP_BASE_URL}/event/uploadPhotos`,
+          headers: { "Content-Type": "application/JSON" },
+          data: formData,
+        }).then((photosResponse) => photosResponse.data),
+        {
+          pending: "Uploading photos...",
+          success: "Photos Uploaded!",
+          error: "There was some error! Please Try again.",
+        }
+      );
+      const updatedEvent = await promise;
       if (updatedEvent) {
-        console.log(updatedEvent);
+        onSubmitProps.resetForm();
+
+        setButtonDisabled(false);
+
         getEvents();
-        toast("Photos Uploaded!.", {
-          type: "success",
-          position: "top-right",
-          autoClose: 3000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: false,
-          draggable: true,
-          progress: undefined,
-          theme: "colored",
-        });
       }
     } catch (error) {
-      toast("There was some error! Please Try again.", {
-        type: "error",
-        position: "top-right",
-        autoClose: 3000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: false,
-        draggable: true,
-        progress: undefined,
-        theme: "colored",
-      });
+      setButtonDisabled(false);
     }
   };
 
@@ -102,29 +92,21 @@ const UploadPhotos = ({ id, getEvents }) => {
                   <Box {...getRootProps()}>
                     <input {...getInputProps()} />
                     {values.photos.length <= 0 && (
-                      <Typography
-                        variant='p'
-                        backgroundColor='#f44336'
-                        color='#fff'
-                        p={1}
-                        borderRadius='5px'
+                      <Button
+                        variant='contained'
+                        color='error'
+                        endIcon={<CloudUploadOutlined />}
                         sx={{
-                          marginLeft: "10px",
-                          display: "flex",
-                          flexDirection: "row",
-                          alignItems: "center",
-                          gap: 0.5,
-                          "&:hover": {
-                            cursor: "pointer",
-                          },
+                          minWidth: "9rem",
+                          color: "#fff",
+                          padding: "auto 0px",
                         }}
                         onClick={() =>
                           window.alert("You can upload up to 5 photos.")
                         }
                       >
-                        UPLOAD PHOTOS
-                        <CloudUploadOutlined />
-                      </Typography>
+                        UPLOAD NOW
+                      </Button>
                     )}
                   </Box>
                 )}
@@ -137,41 +119,28 @@ const UploadPhotos = ({ id, getEvents }) => {
             </Box>
           ) : (
             <Box>
-              <Button type='submit'>
-                <Typography
-                  variant='p'
-                  backgroundColor='#66bb6a'
-                  color='#fff'
-                  p={1}
-                  borderRadius='5px'
-                  sx={{
-                    marginLeft: "5px",
-                    display: "flex",
-                    flexDirection: "row",
-                    alignItems: "center",
-                    gap: 0.5,
-                  }}
-                >
-                  Submit
-                </Typography>
+              <Button
+                disabled={buttonDisabled}
+                type='submit'
+                variant='contained'
+                color='success'
+                sx={{
+                  color: "#fff",
+                  marginRight: "10px",
+                }}
+              >
+                Submit
               </Button>
-              <Button onClick={resetForm}>
-                <Typography
-                  variant='p'
-                  backgroundColor='rgb(255 167 38)'
-                  color='#fff'
-                  p={1}
-                  borderRadius='5px'
-                  sx={{
-                    marginLeft: "5px",
-                    display: "flex",
-                    flexDirection: "row",
-                    alignItems: "center",
-                    gap: 0.5,
-                  }}
-                >
-                  Reset
-                </Typography>
+              <Button
+                variant='contained'
+                color='warning'
+                sx={{
+                  color: "#fff",
+                  padding: "6px 0px",
+                }}
+                onClick={resetForm}
+              >
+                Reset
               </Button>
             </Box>
           )}
