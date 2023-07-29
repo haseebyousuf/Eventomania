1e5ee5322d5922fef1cc06da6ab24f22a45f3be0
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-07-29 12:40:59 +0530

    FIX: removed redux persist

--- client/src/index.js
+++ client/src/index.js
@@ -2,42 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import authReducer from "./state";
 import { configureStore } from "@reduxjs/toolkit";
+import globalReducer from "state";
 import { Provider } from "react-redux";
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import { PersistGate } from "redux-persist/integration/react";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { api } from "state/api";
 
-
-const persistConfig = { key: "root", storage, version: 1 };
-const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  reducer: {
+    global: globalReducer,
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefault) => getDefault().concat(api.middleware),
 });
+setupListeners(store.dispatch);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistStore(store)}>
-        <App />
-      </PersistGate>
+      {/* <PersistGate loading={null} persistor={persistStore(store)}> */}
+      <App />
+      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>
 );
