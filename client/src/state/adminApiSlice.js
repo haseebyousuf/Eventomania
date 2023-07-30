e57a35f21595f8ed305ae457632ade17ee3f14a4
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-07-30 13:53:00 +0530

    create useConvenor, useMember queries and deleteConvenor, deleteMember and ChangePassword Mutation

--- client/src/state/adminApiSlice.js
+++ client/src/state/adminApiSlice.js
@@ -10,12 +10,30 @@ export const adminApiSlice = api.injectEndpoints({
       }),
       providesTags: ["Admins"],
     }),
+    convenors: build.query({
+      query: () => `admin/convenors`,
+      providesTags: ["Admins"],
+    }),
     addConvenor: build.mutation({
       query: (data) => ({
         url: `admin/addConvenor`,
         method: "POST",
         body: data,
       }),
+      providesTags: ["Admins"],
+      invalidatesTags: ["Admins"],
+    }),
+    deleteConvenor: build.mutation({
+      query: (data) => ({
+        url: `admin/deleteConvenor`,
+        method: "POST",
+        body: data,
+      }),
+      providesTags: ["Admins"],
+      invalidatesTags: ["Admins"],
+    }),
+    members: build.query({
+      query: () => `admin/members`,
       providesTags: ["Admins"],
     }),
     addMember: build.mutation({
@@ -25,12 +43,36 @@ export const adminApiSlice = api.injectEndpoints({
         body: data,
       }),
       providesTags: ["Admins"],
+      invalidatesTags: ["Admins"],
+    }),
+    deleteMember: build.mutation({
+      query: (data) => ({
+        url: `admin/deleteMember`,
+        method: "POST",
+        body: data,
+      }),
+      providesTags: ["Admins"],
+      invalidatesTags: ["Admins"],
+    }),
+    changePassword: build.mutation({
+      query: (data) => ({
+        url: `admin/changePassword`,
+        method: "POST",
+        body: data,
+      }),
+      providesTags: ["Admins"],
+      invalidatesTags: ["Admins"],
     }),
   }),
 });
 
 export const {
   useLoginMutation,
+  useConvenorsQuery,
   useAddConvenorMutation,
+  useDeleteConvenorMutation,
+  useMembersQuery,
   useAddMemberMutation,
+  useDeleteMemberMutation,
+  useChangePasswordMutation,
 } = adminApiSlice;
