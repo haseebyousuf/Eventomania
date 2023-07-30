8a64f520e2e73ecf631902ed394fe86eee32b253
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-07-30 13:55:37 +0530

    create unApprovedEvents, approvedEvents queries and approveEvent, deleteEvent and togglePublish Mutation

--- client/src/state/eventApiSlice.js
+++ client/src/state/eventApiSlice.js
@@ -10,13 +10,59 @@ export const eventApiSlice = api.injectEndpoints({
         formData: true,
       }),
       providesTags: ["Events"],
+      invalidatesTags: ["Events"],
     }),
     publishedEvents: build.query({
-      query: () => `events/getPublishedEvents`,
+      query: () => `events/publishedEvents`,
       providesTags: ["Events"],
+    }),
+    unapprovedEvents: build.query({
+      query: () => `events/unapprovedEvents`,
+      providesTags: ["Events"],
+    }),
+    approvedEvents: build.query({
+      query: () => `events/approvedEvents`,
+      providesTags: ["Events"],
+    }),
+    approveEvent: build.mutation({
+      query: (data) => ({
+        url: `events/approveEvent`,
+        method: "POST",
+        body: data,
+        formData: true,
+      }),
+      providesTags: ["Events"],
+      invalidatesTags: ["Events"],
+    }),
+    deleteEvent: build.mutation({
+      query: (data) => ({
+        url: `events/deleteEvent`,
+        method: "POST",
+        body: data,
+        formData: true,
+      }),
+      providesTags: ["Events"],
+      invalidatesTags: ["Events"],
+    }),
+    togglePublish: build.mutation({
+      query: (data) => ({
+        url: `events/togglePublish`,
+        method: "POST",
+        body: data,
+        formData: true,
+      }),
+      providesTags: ["Events"],
+      invalidatesTags: ["Events"],
     }),
   }),
 });
 
-export const { useCreateEventMutation, usePublishedEventsQuery } =
-  eventApiSlice;
+export const {
+  useCreateEventMutation,
+  usePublishedEventsQuery,
+  useUnapprovedEventsQuery,
+  useApprovedEventsQuery,
+  useApproveEventMutation,
+  useDeleteEventMutation,
+  useTogglePublishMutation,
+} = eventApiSlice;
