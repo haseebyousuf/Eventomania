0acd2419bc53562043be6bc2e278fbf5f2082a0f
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-03 21:02:53 +0530

    chore: Update toast error message format

--- client/src/scenes/CommitteeScenes/CommitteeMembers/CommitteeMembers.jsx
+++ client/src/scenes/CommitteeScenes/CommitteeMembers/CommitteeMembers.jsx
@@ -31,17 +31,7 @@ const CommitteeMembers = () => {
       }).unwrap();
       setData(res);
     } catch (error) {
-      toast("There was some error! Please Try again.", {
-        type: "error",
-        position: "top-right",
-        autoClose: 3000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: false,
-        draggable: true,
-        progress: undefined,
-        theme: "colored",
-      });
+      toast.error("There was some error! Please Try again.");
     }
   };
 
