a84d7b4df41193c735fe32df3f1047c12c27a5bd
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-06 22:36:36 +0530

    feat: Add utility for generating and setting JWT token

---
+++ server/utils/generateToken.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+const generateToken = (res, userId) => {
+  try {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "strict",
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default generateToken;
