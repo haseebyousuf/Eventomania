84ba7c4c4b7ff18fd1e58504b5b6b9de38c69ba3
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-07 17:41:02 +0530

    feat: Enhance API configuration in api.js

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -1,7 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.REACT_APP_BASE_URL,
+    credentials: "include",
+  }),
   reducerPath: "adminApi",
   tagTypes: ["Admins", "Events", "Users", "Committees"],
   endpoints: (build) => ({}),
