0f9cac7aa69001b1c4396b427e0b0848c6e6b2f1
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-07 17:49:01 +0530

    feat: Enhance committeeRoutes.js with role-based access control

--- server/routes/committeeRoutes.js
+++ server/routes/committeeRoutes.js
@@ -1,15 +1,15 @@
 import express from "express";
-import { verifyToken } from "../middleware/auth.js";
 import {
   addCommittee,
   getCommittees,
   deleteCommittee,
 } from "../controllers/committeeController.js";
+import { checkRole } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/addCommittee", addCommittee);
-router.get("/getCommittees", getCommittees);
-router.post("/deleteCommittee", deleteCommittee);
+router.post("/addCommittee", checkRole(["admin"]), addCommittee);
+router.get("/getCommittees", checkRole(["admin"]), getCommittees);
+router.post("/deleteCommittee", checkRole(["admin"]), deleteCommittee);
 
 export default router;
