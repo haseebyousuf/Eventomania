e1668a87a1f45892900a0b74eb80e00eeaf4f3fc
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-07 17:34:33 +0530

    Chore: Added credentials middleware for CORS configuration

---
+++ server/middleware/credentials.js
@@ -0,0 +1,11 @@
+import allowedOrigins from "../config/allowedOrigins.js";
+
+const credentials = (req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Credentials", true);
+  }
+  next();
+};
+
+export default credentials;
