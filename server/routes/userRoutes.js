c66314a0a2a982112614bdc0e0d4dc25adb177c8
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-07 17:50:36 +0530

    feat: Enhance userRoutes.js with role-based access control

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { registerStudent,registerFaculty, getUsers } from "../controllers/userController.js";
+import {
+  registerStudent,
+  registerFaculty,
+  getUsers,
+} from "../controllers/userController.js";
+import { checkRole } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 router.post("/registerStudent", registerStudent);
 router.post("/registerFaculty", registerFaculty);
-router.get("/getUsers", getUsers);
-
+router.get("/getUsers", checkRole(["admin", "convenor", "member"]), getUsers);
 
 export default router;
