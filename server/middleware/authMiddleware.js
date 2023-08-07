e3319ea9b85a5c9dcba7905bac064c3026ba4008
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-07 17:47:25 +0530

    refactor: Update authMiddleware.js for role-based authorization consistency

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,30 +1,10 @@
 import jwt from "jsonwebtoken";
 import Admin from "../models/Admin.js";
 
-// export const isAdmin = async (req, res, next) => {
-//   let token;
-//   token = req.cookies.jwt;
-//   console.log(req);
-//   if (token) {
-//     try {
-//       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//       const admin = await Admin.findOne({ _id: decoded.userId }).select("role");
-//       const adminRole = admin ? admin.role : null;
-//       if (!adminRole === "admin") return res.status(401).send("Not Admin");
-//       next();
-//     } catch (error) {
-//       return res.status(401).send("Not Authorized, Invalid Token");
-//     }
-//   } else {
-//     return res.status(401).send("Not Authorized, No Token");
-//   }
-// };
-
 export const checkRole = (roles) => {
   return async (req, res, next) => {
     let token;
     token = req.cookies.jwt;
-    console.log(req.cookies);
     if (token) {
       try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
