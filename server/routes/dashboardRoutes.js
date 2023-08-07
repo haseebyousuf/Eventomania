9663dbbb8180c3e7b533cac5585de7c3f6880893
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-07 17:50:09 +0530

    feat: Enhance dashboardRoutes.js with role-based access control

--- server/routes/dashboardRoutes.js
+++ server/routes/dashboardRoutes.js
@@ -1,11 +1,17 @@
-import express from 'express';
-import {adminDashboardStats, committeeDashboardStats} from "../controllers/dashboardController.js"
+import express from "express";
+import {
+  adminDashboardStats,
+  committeeDashboardStats,
+} from "../controllers/dashboardController.js";
+import { checkRole } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-
-router.get("/adminDashboardStats",adminDashboardStats );
-router.post("/committeeDashboardStats",committeeDashboardStats );
-
+router.get("/adminDashboardStats", checkRole(["admin"]), adminDashboardStats);
+router.post(
+  "/committeeDashboardStats",
+  checkRole(["convenor", "member"]),
+  committeeDashboardStats
+);
 
 export default router;
