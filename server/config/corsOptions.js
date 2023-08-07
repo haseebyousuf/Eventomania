72ad4cd685ecb67505f6053440bf8683e82c8915
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-07 17:43:35 +0530

    feat: Implement CORS options configuration

---
+++ server/config/corsOptions.js
@@ -0,0 +1,15 @@
+import allowedOrigins from "./allowedOrigins.js";
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== 1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  Credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+export default corsOptions;
