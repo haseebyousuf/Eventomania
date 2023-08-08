15c44cc2de074415821497a4bab5aa18d114df10
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-08 20:15:07 +0530

    refactor: remove token state and localStorage handling for token

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -5,9 +5,6 @@ const initialState = {
   user: localStorage.getItem("user")
     ? JSON.parse(localStorage.getItem("user"))
     : null,
-  token: localStorage.getItem("token")
-    ? JSON.parse(localStorage.getItem("token"))
-    : null,
 };
 
 export const globalSlice = createSlice({
@@ -23,15 +20,12 @@ export const globalSlice = createSlice({
     },
     setLogin: (state, action) => {
       state.user = action.payload.user;
-      state.token = action.payload.token;
       localStorage.setItem("user", JSON.stringify(action.payload.user));
-      localStorage.setItem("token", JSON.stringify(action.payload.token));
     },
     setLogout: (state) => {
       state.user = null;
       state.token = null;
       localStorage.removeItem("user");
-      localStorage.removeItem("token");
     },
   },
 });
