8b7467b67b492c5faee9b48db013b101433e9efb
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-08 21:35:31 +0530

    feat: Add Actions component for displaying event details

---
+++ client/src/scenes/CommitteeScenes/UnapprovedEvents/Actions.jsx
@@ -0,0 +1,39 @@
+import { Box, IconButton, Tooltip } from "@mui/material";
+import { useState } from "react";
+import RemoveRedEyeOutlinedIcon from "@mui/icons-material/RemoveRedEyeOutlined";
+
+import EventDialog from "components/EventDialog";
+
+const Actions = ({ params }) => {
+  //state
+  const [openDialog, setOpenDialog] = useState(false);
+
+  //handlers
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+  };
+
+  const handleOpenDialog = () => {
+    setOpenDialog(true);
+  };
+
+  return (
+    <Box>
+      <EventDialog
+        handleOpenDialog={handleOpenDialog}
+        handleCloseDialog={handleCloseDialog}
+        openDialog={openDialog}
+        setOpenDialog={setOpenDialog}
+        params={params}
+        showOrder={true}
+      />
+      <Tooltip title='View Event Details'>
+        <IconButton onClick={handleOpenDialog}>
+          <RemoveRedEyeOutlinedIcon color='info' />
+        </IconButton>
+      </Tooltip>
+    </Box>
+  );
+};
+
+export default Actions;
