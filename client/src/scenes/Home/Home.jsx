b4290776fb2ad092151faaf56cb9d15f65211796
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-11 22:32:46 +0530

    fix: git config core.ignorecase false

---
+++ client/src/scenes/Home/Home.jsx
@@ -0,0 +1,86 @@
+import { Box } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import moment from "moment";
+
+import HomeNavbar from "../../components/HomeNavbar";
+import EventContainer from "components/EventContainer";
+import { usePublishedEventsQuery } from "state/eventApiSlice";
+
+const Home = () => {
+  const [upcomingEvents, setUpcomingEvents] = useState(null);
+  const [pastEvents, setPastEvents] = useState(null);
+  const [filteredPastEvents, setFilteredPastEvents] = useState(null);
+  const [filteredUpcomingEvents, setFilteredUpcomingEvents] = useState(null);
+  const { data: events, isLoading } = usePublishedEventsQuery();
+
+  useEffect(() => {
+    if (!isLoading && events && events.length > 0) {
+      setUpcomingEvents(
+        events.filter((item) => {
+          if (
+            moment(new Date(item.startDate)).isAfter(moment()) ||
+            moment(new Date(item.startDate)).isSame(
+              moment(),
+              "day",
+              "month",
+              "year"
+            )
+          ) {
+            return true;
+          } else {
+            return false;
+          }
+        })
+      );
+      setFilteredUpcomingEvents(
+        events.filter(
+          (item) =>
+            moment(new Date(item.startDate)).isAfter(moment()) ||
+            moment(new Date(item.startDate)).isSame(
+              moment(),
+              "day",
+              "month",
+              "year"
+            )
+        )
+      );
+      setPastEvents(
+        events.filter((item) =>
+          moment(new Date(item.startDate)).isBefore(moment())
+        )
+      );
+
+      setFilteredPastEvents(
+        events.filter((item) =>
+          moment(new Date(item.startDate)).isBefore(moment())
+        )
+      );
+    }
+  }, [events, isLoading]);
+
+  return (
+    <Box
+      sx={{
+        height: "100vh",
+      }}
+    >
+      <HomeNavbar />
+      <EventContainer
+        title='UPCOMING EVENTS'
+        filteredEvents={filteredUpcomingEvents}
+        setFilteredEvents={setFilteredUpcomingEvents}
+        events={upcomingEvents}
+        isPast={false}
+      />
+      <EventContainer
+        title='PAST EVENTS'
+        filteredEvents={filteredPastEvents}
+        setFilteredEvents={setFilteredPastEvents}
+        events={pastEvents}
+        isPast={true}
+      />
+    </Box>
+  );
+};
+
+export default Home;
---
+++ client/src/scenes/Login/LoginForm.jsx
@@ -0,0 +1,168 @@
+import React from "react";
+import {
+  Box,
+  Card,
+  CardContent,
+  TextField,
+  Button,
+  Typography,
+  useTheme,
+  CardActions,
+} from "@mui/material";
+import { Formik } from "formik";
+import * as yup from "yup";
+import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+
+import { useLoginMutation } from "state/adminApiSlice";
+import { setLogin } from "state";
+
+const loginSchema = yup.object().shape({
+  email: yup.string().email("invalid email").required("required"),
+  password: yup.string().required("No password provided."),
+});
+
+const initialValuesLogin = {
+  email: "",
+  password: "",
+};
+
+const LoginForm = () => {
+  const dispatch = useDispatch();
+  const theme = useTheme();
+
+  //rtk query
+  const [login, { isLoading }] = useLoginMutation();
+
+  //handlers
+  const handleFormSubmit = async (values, onSubmitProps) => {
+    try {
+      const res = await login(values).unwrap();
+      dispatch(setLogin({ ...res }));
+      toast.success("Welcome to Dashboard");
+    } catch (error) {
+      toast.error(error?.data?.msg || "Server Error");
+    }
+  };
+
+  return (
+    <Formik
+      onSubmit={handleFormSubmit}
+      initialValues={initialValuesLogin}
+      validationSchema={loginSchema}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleBlur,
+        handleChange,
+        handleSubmit,
+        setFieldValue,
+        resetForm,
+      }) => (
+        <form onSubmit={handleSubmit}>
+          <Box>
+            <Card
+              sx={{
+                padding: "0rem 1.8rem",
+                backgroundImage: "none",
+                backgroundColor: theme.palette.background.alt,
+              }}
+            >
+              <CardContent>
+                <Box
+                  display='flex'
+                  justifyContent='center'
+                  alignItems='flex-start'
+                  flexDirection='column'
+                  p='0.5rem'
+                >
+                  <Typography
+                    fontSize='1.8rem'
+                    textDecoration='underline'
+                    fontWeight='bold'
+                    p='1rem 0rem 1rem 0rem'
+                    color={theme.palette.secondary.main}
+                  >
+                    EVENTOMANIA
+                  </Typography>
+                  <Typography
+                    fontSize='1.5rem'
+                    textDecoration='underline'
+                    fontWeight='bold'
+                    color={theme.palette.secondary}
+                  >
+                    Hello Admin 👋
+                  </Typography>
+                  <Typography
+                    fontSize='1rem'
+                    textDecoration='underline'
+                    color={theme.palette.secondary}
+                    paddingBottom='0.6rem'
+                  >
+                    Sign In to access your account
+                  </Typography>
+
+                  {/* <AdminPanelSettingsOutlinedIcon fontSize="large" /> */}
+                </Box>
+                <TextField
+                  id='email'
+                  autoComplete='off'
+                  color='secondary'
+                  label='Email'
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  helperText={touched.email ? errors.email : ""}
+                  error={touched.email && Boolean(errors.email)}
+                  margin='dense'
+                  variant='outlined'
+                  fullWidth
+                />
+                <TextField
+                  autoComplete='off'
+                  type='password'
+                  color='secondary'
+                  id='password'
+                  label='Password'
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  helperText={touched.password ? errors.password : ""}
+                  error={touched.password && Boolean(errors.password)}
+                  margin='dense'
+                  variant='outlined'
+                  fullWidth
+                />
+              </CardContent>
+              <CardActions
+                display='flex'
+                sx={{
+                  marginBottom: "1rem",
+                  justifyContent: "center",
+                }}
+              >
+                <Button
+                  variant='contained'
+                  type='submit'
+                  disabled={isLoading}
+                  sx={{
+                    color: "black",
+                    fontWeight: "bold",
+                  }}
+                  size='large'
+                  color='secondary'
+                >
+                  Sign In
+                </Button>
+              </CardActions>
+            </Card>
+          </Box>
+        </form>
+      )}
+    </Formik>
+  );
+};
+
+export default LoginForm;
