43f0a3c106cc7874137e64c51b184a2aae8b7fa2
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-11 23:14:51 +0530

    refactor: set same site to none

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -8,7 +8,7 @@ const generateToken = (res, userId) => {
     res.cookie("jwt", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     });
   } catch (error) {
