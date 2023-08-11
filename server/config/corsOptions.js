91c41bec5d1a0086881aa39e286a1babdaf07705
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-11 22:57:38 +0530

    refactor: add methods to cors options

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -8,6 +8,7 @@ const corsOptions = {
       callback(new Error("Not allowed by CORS"));
     }
   },
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
   optionsSuccessStatus: 200,
 };
