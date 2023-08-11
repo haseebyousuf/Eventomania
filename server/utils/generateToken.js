26779137cd9af52111e2c018fc99294e430ff2ac
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-11 23:07:20 +0530

    refactor: set secure true for cookies

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -7,7 +7,7 @@ const generateToken = (res, userId) => {
     });
     res.cookie("jwt", token, {
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: "strict",
       maxAge: 24 * 60 * 60 * 1000,
     });
