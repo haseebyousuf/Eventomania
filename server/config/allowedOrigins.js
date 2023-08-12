87faf6a39a3c6d2a31de9c7b67fd4f1875cc240a
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-12 13:27:44 +0530

    refactor: allow netlify domain

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
   "https://eventomania.onrender.com/",
+  "https://eventomania.netlify.app/",
   "http://127.0.0.1:5500",
   "http://localhost:3500",
   "http://localhost:3000",
