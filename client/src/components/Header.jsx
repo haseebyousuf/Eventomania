a2178bbcba7415b0ec625935f8f59a9ab0da7f3b
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-13 14:12:49 +0530

    refactor: Enhance Header component with animation and responsiveness

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,27 +1,36 @@
-import { Typography, Box, useTheme } from "@mui/material";
+import { Typography, Box, useTheme, useMediaQuery } from "@mui/material";
+import { motion } from "framer-motion";
 import React from "react";
 
 const Header = ({ title, subtitle }) => {
   const theme = useTheme();
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
 
   return (
-    <Box>
-      <Typography
-        variant='h2'
-        color={theme.palette.secondary[100]}
-        fontWeight='bold'
-        sx={{ mb: "5px" }}
+    <Box overflow='hidden'>
+      <Box
+        component={motion.div}
+        initial={{ y: "-100%" }}
+        animate={{ y: "0%" }}
+        transition={{ delay: 0.4, duration: 0.5 }}
       >
-        {title}
-      </Typography>
-      <Typography
-        sx={{ mb: "5px" }}
-        variant='h5'
-        fontWeight='bold'
-        color={theme.palette.secondary[300]}
-      >
-        {subtitle}
-      </Typography>
+        <Typography
+          variant={isNonMobile ? "h2" : "h3"}
+          color={theme.palette.secondary[100]}
+          fontWeight='bold'
+          sx={{ mb: "5px" }}
+        >
+          {title}
+        </Typography>
+        <Typography
+          sx={{ mb: "5px" }}
+          variant={isNonMobile ? "h5" : "h6"}
+          fontWeight='bold'
+          color={theme.palette.secondary[300]}
+        >
+          {subtitle}
+        </Typography>
+      </Box>
     </Box>
   );
 };
