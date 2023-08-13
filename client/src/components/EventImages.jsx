5195be2176583f8e63f36b8d1cf9b47dec8f0bbc
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-13 14:11:10 +0530

    refactor: Update EventImages styling and presentation

--- client/src/components/EventImages.jsx
+++ client/src/components/EventImages.jsx
@@ -1,8 +1,19 @@
-import { Box } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardContent,
+  Typography,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import { motion } from "framer-motion";
 import React from "react";
 import ImageGallery from "react-image-gallery";
 
 const EventImages = ({ photos }) => {
+  const theme = useTheme();
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+
   //set image data
   const images = photos.map((photo) => {
     return {
@@ -13,13 +24,47 @@ const EventImages = ({ photos }) => {
 
   return (
     <Box sx={{ display: "block", overflow: "auto" }} marginBottom='1rem'>
-      <ImageGallery
-        items={images}
-        showNav={true}
-        showThumbnails={false}
-        autoPlay={true}
-        showPlayButton={false}
-      />
+      <Card
+        sx={{
+          padding: "0rem 0.7rem",
+          borderRadius: "0.55rem",
+          backgroundImage: "none",
+          backgroundColor: theme.palette.background.alt,
+        }}
+      >
+        <CardContent>
+          <Box
+            display='flex'
+            justifyContent='center'
+            alignItems='flex-start'
+            flexDirection='column'
+          >
+            <Typography
+              fontSize={isNonMobile ? "1.6rem" : "1.2rem"}
+              textDecoration='underline'
+              fontWeight='bold'
+              p='0.3rem 0rem 1rem 0rem'
+              color='#d12121'
+            >
+              EVENT CONCLUDED!
+            </Typography>
+          </Box>
+          <Box
+            component={motion.div}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1, ease: "easeInOut" }}
+          >
+            <ImageGallery
+              items={images}
+              showNav={true}
+              showThumbnails={false}
+              autoPlay={true}
+              showPlayButton={false}
+            />
+          </Box>
+        </CardContent>
+      </Card>
     </Box>
   );
 };
