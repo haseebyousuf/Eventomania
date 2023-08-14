5f960821ae8e8864cadfc280dfed2620efda684c
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-14 19:16:41 +0530

    feat: Add initial form values to initialValues.js

---
+++ client/src/utils/initialValues.js
@@ -0,0 +1,69 @@
+export const loginInitialValues = {
+  email: "",
+  password: "",
+};
+
+export const changePasswordInitialValues = {
+  currentPassword: "",
+  newPassword: "",
+  cNewPassword: "",
+};
+
+export const eventInitialValues = {
+  name: "",
+  startDate: null,
+  endDate: null,
+  venue: "",
+  description: "",
+  banner: "",
+  order: "",
+};
+
+export const committeeMemberInitialValues = {
+  memberName: "",
+  memberEmail: "",
+  memberPassword: "",
+  mobile: "",
+};
+
+export const memberInitialValues = {
+  memberName: "",
+  memberEmail: "",
+  memberPassword: "",
+  committee: "",
+  mobile: "",
+};
+
+export const convenorInitialValues = {
+  name: "",
+  email: "",
+  password: "",
+  committee: "",
+  mobile: "",
+};
+
+export const committeeInitialValues = {
+  name: "",
+  description: "",
+};
+
+export const studentInitialValues = {
+  name: "",
+  regNo: "",
+  mobileNo: "",
+  email: "",
+  courseSemesterDept: "",
+  department: "",
+};
+
+export const facultyInitialValues = {
+  name: "",
+  employeeId: "",
+  mobileNo: "",
+  email: "",
+  department: "",
+};
+
+export const reportInitialValue = {
+  report: "",
+};
