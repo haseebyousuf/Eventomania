22a0e3c33efa27c58ca7802133a6f09387d768e2
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-16 00:18:01 +0530

    feat: add motion animations to EventInfoBox component

--- client/src/components/EventInfoBox.jsx
+++ client/src/components/EventInfoBox.jsx
@@ -1,12 +1,25 @@
 import { Box, Divider, Grid, Typography, useMediaQuery } from "@mui/material";
+import { motion } from "framer-motion";
 import React from "react";
 
-const EventInfoBox = ({ icon, title, value, last }) => {
+const EventInfoBox = ({ icon, title, value, last, index }) => {
   const isNonMobile = useMediaQuery("(min-width: 600px)");
 
   return (
     <>
-      <Grid item xs={12} sm={12} md={4} lg={4}>
+      <Grid
+        component={motion.div}
+        initial={{ y: 20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        viewport={{ once: true }}
+        transition={{ delay: 0.15 * index }}
+        exit={{ y: 20, opacity: 0 }}
+        item
+        xs={12}
+        sm={12}
+        md={4}
+        lg={4}
+      >
         <Box display='flex' gap={2}>
           <Box display='flex' gap={1}>
             {icon}
