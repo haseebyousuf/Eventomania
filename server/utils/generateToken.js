5e068400e33cc2cc6ee6c0838ecfcdd50379bf7e
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-16 00:04:49 +0530

    feat: Extend JWT expiration to 30 days

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 const generateToken = (res, userId) => {
   try {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-      expiresIn: "1d",
+      expiresIn: "30d",
     });
     res.cookie("jwt", token, {
       httpOnly: true,
