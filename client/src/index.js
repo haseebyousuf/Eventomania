91cb745422503c84cdb05a551c1922024de43899
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-17 22:41:37 +0530

    refactor: Remove commented  code from index.js

--- client/src/index.js
+++ client/src/index.js
@@ -21,9 +21,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistStore(store)}> */}
       <App />
-      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>
 );
