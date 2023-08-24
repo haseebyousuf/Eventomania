76e2a69fc72d68fec478c2f3473c75c03c4b162d
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-08-25 00:48:16 +0530

    refactor: fix shimmer style

--- client/src/components/EventDetailsShimmer.jsx
+++ client/src/components/EventDetailsShimmer.jsx
@@ -1,14 +1,32 @@
-import { Box, Divider, Grid, Skeleton, useMediaQuery } from "@mui/material";
+import {
+  Box,
+  Card,
+  Divider,
+  Grid,
+  Skeleton,
+  useMediaQuery,
+} from "@mui/material";
 import React from "react";
+import FlexBetween from "./FlexBetween";
 
 const EventDetailsShimmer = () => {
   const isNonMobile = useMediaQuery("(min-width: 600px)");
 
   return (
     <Box>
-      <Grid width='90%' margin='auto' columnSpacing={4} container>
-        <Grid sx={{ marginBottom: "1rem" }} item xs={12} sm={12} md={7} lg={8}>
-          <Skeleton height='40vh' animation='wave' />
+      <Grid width='90%' margin='auto' container mt={2}>
+        <Grid
+          sx={{ marginBottom: "1rem" }}
+          item
+          xs={12}
+          sm={12}
+          md={7}
+          lg={8}
+          rowSpacing={2}
+        >
+          <Box marginTop={5}>
+            <Skeleton variant='rounded' height={400} animation='wave' />
+          </Box>
           <Skeleton height='10vh' animation='wave' />
 
           <Divider
@@ -21,11 +39,20 @@ const EventDetailsShimmer = () => {
           <Skeleton height='10vh' animation='wave' />
           <Skeleton height='10vh' animation='wave' />
         </Grid>
-        <Grid item xs={12} sm={12} md={5} lg={4}>
-          <Skeleton height='80vh' />
+        <Grid sx={{}} item xs={12} sm={12} md={5} lg={4}>
+          <Box
+            marginLeft='2rem'
+            position={isNonMobile && "sticky"}
+            top={isNonMobile && "5rem"}
+          >
+            <Card>
+              <Skeleton variant='rounded' sx={{ padding: 0 }} height={550} />
+            </Card>
+          </Box>
           {!isNonMobile && <Skeleton height='20vh' animation='wave' />}
         </Grid>
       </Grid>
+      <FlexBetween></FlexBetween>
     </Box>
   );
 };
